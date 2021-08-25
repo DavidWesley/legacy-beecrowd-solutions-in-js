5bb33f074cd6f92f19298c1931541671772891ce
David Wesley <david.wesley755@outlook.com>
2021-08-25 02:58:05 -0300

    Add 1278.js File (Align text from right) SOLVED

---
+++ strings/1278.js
@@ -0,0 +1,31 @@
+const { readFileSync } = require("fs")
+const input = readFileSync("/dev/stdin", "utf8")
+
+function alignFromRight(texts = ['']) {
+	const biggestTextLength = Math.max(...texts.map(text => text.length))
+	return texts.map(text => text.padStart(biggestTextLength, ' ')).join('\n')
+}
+
+/** @param {string} spacedText*/
+const deepTrimSpaces = (spacedText) => spacedText.replace(/\s+/g, ' ').trim()
+
+function main() {
+	const responses = []
+
+	const quantitiesTexts = input.match(/(?<number>\d+)/g)
+	const textsList = input.match(/(?<texts>[a-zA-Z ]+$)/gm)
+
+	for (const quantity of quantitiesTexts) {
+		if (quantity === '0') break
+
+		const currentSelectedWords = textsList.splice(0, +quantity)
+		const currentTrimmedWords = currentSelectedWords.map(deepTrimSpaces)
+		const currentAlignedWords = alignFromRight(currentTrimmedWords)
+
+		responses.push(currentAlignedWords)
+	}
+
+	console.log(responses.join('\n\n'))
+}
+
+main()
