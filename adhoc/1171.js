7f74736e1b3bee0da5acb2ebd831fa18e4ae3da9
David Wesley <david.wesley755@outlook.com>
2021-08-30 00:50:39 -0300

    Starting ADHOC folder

---
+++ adhoc/1171.js
@@ -0,0 +1,23 @@
+const { readFileSync } = require("fs")
+const [numTestCases, ...numbers] = readFileSync("/dev/stdin", "utf8").split('\n')
+
+function getFrequenciesInQueue(list) {
+	return list.reduce((frequencies, key) => (frequencies[`${key}`] = ++frequencies[`${key}`] || 1, frequencies), {})
+}
+
+function main() {
+	const responses = []
+
+	const numbersLists = numbers.slice(0, +numTestCases).map(num => Number.parseInt(num, 10))
+	const frequenciesList = getFrequenciesInQueue(numbersLists)
+
+	for (const [key, frequency] of Object.entries(frequenciesList)) {
+		responses.push(
+			`${key} aparece ${frequency} vez(es)`
+		)
+	}
+
+	console.log(`${responses.join('\n')}`)
+}
+
+main()
---
+++ adhoc/1285.js
@@ -0,0 +1,36 @@
+const { readFileSync } = require("fs")
+const input = readFileSync("/dev/stdin", "utf8").split('\n')
+
+function createInterval(min = 0, max = min, step = 1) {
+	step = step >= 1 ? step : 1
+	const size = Math.ceil((max - min + 1) / step)
+
+	return Array.from({ length: size }, (_, i) => min + step * i)
+}
+
+/** @param {number} num */
+
+function areAllDistinctDigits(num) {
+	const digits = [...`${num}`]
+	const uniqueDigits = [...new Set(digits)].join('')
+
+	return uniqueDigits === num.toString()
+}
+
+function main() {
+	const responses = []
+	const limitsPairs = input.map(limits => limits.split(' ').map(num => Number.parseInt(num, 10)))
+
+	for (const [min, max] of limitsPairs) {
+		if (isNaN(min) || isNaN(max)) break // EOFile Condition Verification
+
+		const interval = createInterval(min, max, 1)
+		const numsWithUniquesDigits = interval.filter(areAllDistinctDigits)
+
+		responses.push(numsWithUniquesDigits.length)
+	}
+
+	console.log(`${responses.join('\n')}`)
+}
+
+main()
