cb8c9909af11e82176e07e18959ad7b8483bf41c
David Wesley <david.wesley755@outlook.com>
2021-09-29 18:54:48 -0300

    Add File: 3046.js
    Adding file: 3046.js

---
+++ beginner/3046.js
@@ -0,0 +1,13 @@
+const { readFileSync } = require("fs")
+const [num] = readFileSync("/dev/stdin", "utf8").split("\n")
+
+const dominosQuant = (n) => (n + 1) * (n + 2) / 2
+
+function main() {
+	const N = Number.parseInt(num, 10)
+	const dominos = dominosQuant(N)
+
+	console.log(`${dominos}`)
+}
+
+main()
