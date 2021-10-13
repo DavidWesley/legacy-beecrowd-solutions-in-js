3bc6e998ef84ec86b676ff42a67b682e94d5aaee
David Wesley <david.wesley755@outlook.com>
2021-10-13 01:40:37 -0300

    new ConvertBase problem

---
+++ maths/1869.js
@@ -0,0 +1,23 @@
+const { readFileSync } = require("fs")
+const input = readFileSync("/dev/stdin", "utf8").split("\n")
+
+const ConvertBase = (num) => ({
+	from: (baseFrom) => ({
+		to: (baseTo) => parseInt(num, baseFrom).toString(baseTo),
+	}),
+})
+
+function main() {
+	const responses = []
+
+	for (const num of input) {
+		const base32 = ConvertBase(num).from(10).to(32).toUpperCase()
+		responses.push(base32)
+
+		if (num === "0") break // EOFile Condition Verification
+	}
+
+	console.log(`${responses.join("\n")}`)
+}
+
+main()
