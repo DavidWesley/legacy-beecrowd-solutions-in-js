dcd34f3cda84d75634b683ced0e3efe75e49c650
David Wesley <david.wesley755@outlook.com>
2021-10-13 01:41:17 -0300

    Adding 1693.js SOLVED

---
+++ geometry/1693.js
@@ -0,0 +1,25 @@
+const { readFileSync } = require("fs")
+const input = readFileSync("/dev/stdin", "utf8")
+	.split("\n")
+	.map((line) => line.split(" ").slice(0, 4).map(Number.parseFloat))
+
+const ObliqueThrow = {
+	maxHorizontalDistance: (velocity, height, acceleration = 9.8) => {
+		return (velocity / acceleration) * Math.sqrt(Math.pow(velocity, 2) + 2 * acceleration * height)
+	}
+}
+
+function main() {
+	const responses = []
+
+	for (const [R, H, V, G] of input) {
+		if ([R, H, V, G].includes(NaN)) break // EOFile Condition
+
+		const maxDist = ObliqueThrow.maxHorizontalDistance(V, H, G)
+		responses.push(maxDist >= R ? 'Y' : 'N')
+	}
+
+	console.log(`${responses.join("\n")}`)
+}
+
+main()
