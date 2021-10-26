7f8f1558c64a7dea4ba28811ab91aae4cf0bdd7c
David Wesley <david.wesley755@outlook.com>
2021-10-26 02:52:27 -0300

    Refactor 2693.js

--- structure/2693.0.js
+++ structure/2693.0.js
@@ -1,42 +1,35 @@
 const { readFileSync } = require("fs")
 const input = readFileSync("/dev/stdin", "utf8").split("\n")
 
-/** @typedef {{ name: string, region: string, distance: number }} studentModel */
+/** @typedef {{ name: string, region: string, distance: string }} studentModel */
 
 /** @param {studentModel[]} studentsList */
 
 function sortStudentsList(studentsList) {
-	return studentsList.sort((a, b) => {
-		if (a.distance !== b.distance) return a.distance - b.distance
-		else if (a.region !== b.region)
-			return a.region.localeCompare(b.region, "en-US")
-		else return a.name.localeCompare(b.name, "en-US")
+	return studentsList.sort((studentA, studentB) => {
+		if (studentA.distance !== studentB.distance) return studentA.distance.localeCompare(studentB.distance, "en-US", { numeric: true })
+		else if (studentA.region !== studentB.region) return studentA.region.localeCompare(studentB.region, "en-US")
+		else return studentA.name.localeCompare(studentB.name, "en-US")
 	})
 }
 
 function main() {
 	while (input.length > 0) {
-		const Q = Number.parseInt(input.shift(), 10) //=
+		const Q = Number.parseInt(input.shift(), 10)
 
 		if (isNaN(Q)) break // EOFile Condition Verification
 
-		const students = Array.from({ length: Q }).reduce((studentsList) => {
-			const [name, region, dist] = input.shift().split(" ")
+		const students = Array.from({ length: Q }).reduce(studentsList => {
+			const [name, region, distance] = input.shift().split(" ")
 
-			const studentProps = {
-				name: name,
-				region: region,
-				distance: Number.parseInt(dist, 10),
-			}
-
-			studentsList.push({ ...studentProps })
+			studentsList.push({ name, region, distance })
 
 			return studentsList
 		}, [])
 
 		console.log(
 			sortStudentsList(students)
-				.map((student) => student.name)
+				.map(student => student.name)
 				.join("\n")
 		)
 	}
