41ad6abfd7899e627d88238fb9a4452e038f67bd
David Wesley <david.wesley755@outlook.com>
2021-10-30 04:27:20 -0300

    Update 1846.js
    Fixing memory leak problem

--- strings/1846.js
+++ strings/1846.js
@@ -1,13 +1,14 @@
 const { readFileSync } = require("fs")
 const input = readFileSync("/dev/stdin", "utf8").split("\n").map(int => Number.parseInt(int, 10))
 
+const { converter } = convertNumberToPortugueseNomenclature()
+
 function main() {
 	const responses = []
-	const converterInstance = convertNumberToPortugueseNomenclature()
 
 	for (const num of input) {
 		if (isNaN(num)) break // EOFile Condition Verification
-		responses.push(converterInstance(num))
+		responses.push(converter(num))
 	}
 
 	console.log(`${responses.join("\n")}`)
@@ -16,7 +17,7 @@ function main() {
 main()
 
 function convertNumberToPortugueseNomenclature() {
-	const unidades = [
+	const unitsNames = [
 		"",
 		"um",
 		"dois",
@@ -38,7 +39,7 @@ function convertNumberToPortugueseNomenclature() {
 		"dezoito",
 		"dezenove",
 	]
-	const dezenas = [
+	const dozensNames = [
 		"",
 		"dez",
 		"vinte",
@@ -51,7 +52,7 @@ function convertNumberToPortugueseNomenclature() {
 		"noventa",
 		"cem",
 	]
-	const centenas = [
+	const hundredsNames = [
 		"",
 		"cento",
 		"duzentos",
@@ -64,9 +65,9 @@ function convertNumberToPortugueseNomenclature() {
 		"novecentos",
 	]
 
-	const mapUnitsNames = new Map(Object.entries(unidades))
-	const mapDozensNames = new Map(Object.entries(dezenas))
-	const mapHundrendsNames = new Map(Object.entries(centenas))
+	const mapUnitsNames = new Map(Object.entries(unitsNames))
+	const mapDozensNames = new Map(Object.entries(dozensNames))
+	const mapHundrendsNames = new Map(Object.entries(hundredsNames))
 
 	const numberFormatterInstance = new Intl.NumberFormat('en-US', {
 		style: "decimal",
@@ -76,48 +77,49 @@ function convertNumberToPortugueseNomenclature() {
 	})
 
 
-	return function converter(/** @type {number | bigint} */ num, customJoinCharacter = "e") {
+	function convertOrderNumberIntoTextName(order = "000", customJoinCharacter, orderTermination = { single: "", plural: "" }) {
+		const num = Number.parseInt(order, 10)
+		const terminationText = ` ${num >= 2 ? orderTermination.plural : orderTermination.single}`
+
+		if (num === 0)
+			return ""
+
+		else if (num === 100)
+			return `${customJoinCharacter} ${mapDozensNames.get("10").concat(terminationText)}`
+
+		else if (num % 100 === 0 && num !== 0)
+			return `${customJoinCharacter} ${mapHundrendsNames.get(order[0])}${terminationText}`
+
+		else
+			return [...order].map((value, index, arr) => {
+				if (index === 0)
+					return `${mapHundrendsNames.get(value).concat(value !== "0" ? " " : "")}${customJoinCharacter}`
+
+				else if (index === 1)
+					return ["0", "1"].includes(value) ? "" : `${mapDozensNames.get(value)}`
+
+				else
+					if (arr[index - 1] === "1") return mapUnitsNames.get(`1${value}`)
+					else return `${value === "0" || arr[index - 1] === "0" ? "" : customJoinCharacter + " "}${mapUnitsNames.get(value)}`
+
+			})
+				.filter(value => Boolean(value))
+				.join(" ")
+				.replace(RegExp(`(\\s+${customJoinCharacter})$`, 'g'), "")
+				.concat(terminationText)
+	}
+
+
+	function converter(/** @type {number | bigint} */ num, customJoinCharacter = "e") {
 		if (num === 0) return "zero"
 
-		const formatedNumber = numberFormatterInstance.format(num)
+		const [simpleOrderNum, thounsandOrderNum, milionOrderNum] = numberFormatterInstance
+			.format(num)
+			.split(/[\,]/).reverse()
 
-		const [simpleOrderNum, thounsandOrderNum, milionOrderNum] = formatedNumber.split(/[\,]/).reverse()
-
-		function convertOrderNumberIntoTextName(order = "000", orderTermination = { single: "", plural: "" }) {
-			const num = Number.parseInt(order, 10)
-			const terminationText = ` ${num >= 2 ? orderTermination.plural : orderTermination.single}`
-
-			if (num === 0)
-				return ""
-
-			else if (num === 100)
-				return `${customJoinCharacter} ${mapDozensNames.get("10").concat(terminationText)}`
-
-			else if (num % 100 === 0 && num !== 0)
-				return `${customJoinCharacter} ${mapHundrendsNames.get(order[0])}${terminationText}`
-
-			else
-				return [...order].map((value, index, arr) => {
-					if (index === 0)
-						return `${mapHundrendsNames.get(value).concat(value !== "0" ? " " : "")}${customJoinCharacter}`
-
-					else if (index === 1)
-						return ["0", "1"].includes(value) ? "" : `${mapDozensNames.get(value)}`
-
-					else
-						if (arr[index - 1] === "1") return mapUnitsNames.get(`1${value}`)
-						else return `${value === "0" || arr[index - 1] === "0" ? "" : customJoinCharacter + " "}${mapUnitsNames.get(value)}`
-
-				})
-					.filter(value => Boolean(value))
-					.join(" ")
-					.replace(RegExp(`(\\s+${customJoinCharacter})$`, 'g'), "")
-					.concat(terminationText)
-		}
-
-		const simpleOrderText = convertOrderNumberIntoTextName(simpleOrderNum)
-		const thousandOrderText = convertOrderNumberIntoTextName(thounsandOrderNum, { plural: "mil", single: "mil" })
-		const milionOrderText = convertOrderNumberIntoTextName(milionOrderNum, { plural: "milhões", single: "milhão" })
+		const simpleOrderText = convertOrderNumberIntoTextName(simpleOrderNum, customJoinCharacter)
+		const thousandOrderText = convertOrderNumberIntoTextName(thounsandOrderNum, customJoinCharacter, { plural: "mil", single: "mil" })
+		const milionOrderText = convertOrderNumberIntoTextName(milionOrderNum, customJoinCharacter, { plural: "milhões", single: "milhão" })
 
 		const portugueseNomenclatureText = [milionOrderText, thousandOrderText, simpleOrderText]
 			.join(" ")
@@ -127,4 +129,6 @@ function convertNumberToPortugueseNomenclature() {
 
 		return portugueseNomenclatureText
 	}
+
+	return { converter }
 }
