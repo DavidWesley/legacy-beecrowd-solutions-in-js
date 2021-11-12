f84e590b5dfa1b9623e74d47253ac551cc0a41d7
David Wesley <david.wesley755@outlook.com>
2021-11-12 03:38:30 -0300

    maths: 2232.js
    Resolvendo o problema do Triângulo de Pascal

---
+++ maths/2232.js
@@ -0,0 +1,14 @@
+const { readFileSync } = require("fs")
+const [numCases, ...linesList] = readFileSync("/dev/stdin", "utf8").split('\n').map(line => Number.parseInt(line, 10))
+
+const getFinitePGSum = (initVal, tax, numElements) => initVal * (1 - Math.pow(tax, numElements)) / (1 - tax)
+
+function main() {
+	const responses = linesList
+		.slice(0, +numCases)
+		.map(numLines => getFinitePGSum(1, 2, numLines))
+
+	console.log(`${responses.join('\n')}`)
+}
+
+main()
