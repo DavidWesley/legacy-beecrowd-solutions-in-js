2fa2f9f4a355077b7c2c565e90146a71798224ca
David Wesley <david.wesley755@outlook.com>
2021-11-13 00:18:36 -0300

    Create 1930.js

---
+++ beginner/1930.js
@@ -0,0 +1,9 @@
+const { readFileSync } = require("fs")
+const sockets = readFileSync("/dev/stdin", "utf8").split(' ').slice(0, 4).map(value => Number.parseInt(value, 10))
+
+const sumValues = (/** @type {number[]} */[...values], initialValue = 0) => values.reduce((acc, cur) => acc + cur, initialValue)
+
+const totalSum = sumValues(sockets)
+const availableSockets = totalSum - (sockets.length - 1)
+
+console.log(`${availableSockets}`)
