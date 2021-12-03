0187d211bdc72dd5a782666e2084867fddf14252
David Wesley <david.wesley755@outlook.com>
2021-12-03 16:41:55 -0300

    Create 2374.js

---
+++ adhoc/2374.js
@@ -0,0 +1,8 @@
+const { readFileSync } = require("fs")
+const [desiredPressure, pressureRead] = readFileSync("/dev/stdin", "utf8")
+	.split("\n")
+	.slice(0, 2)
+	.map((pressure) => Number.parseInt(pressure, 10))
+
+const pressureDifference = desiredPressure - pressureRead
+console.log(`${pressureDifference}`)
