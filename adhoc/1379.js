35c4b914e4bb9eef1df5ece94e26d3ed09580ea7
David Wesley <david.wesley755@outlook.com>
2021-12-06 17:15:55 -0300

    Create 1379.js

---
+++ adhoc/1379.js
@@ -0,0 +1,15 @@
+const { readFileSync } = require("fs")
+const input = readFileSync("/dev/stdin", "utf8").split("\n").map((line) => line.split(" "))
+
+function main() {
+	const responses = []
+
+	for (const [A, B] of input) {
+		if (A === "0" && B === "0") break
+		responses.push(2 * (+A) - (+B))
+	}
+
+	console.log(`${responses.join("\n")}`)
+}
+
+main()
