2b05204dc73e26c688123429443ec1f65718b62c
David Wesley <david.wesley755@outlook.com>
2021-12-07 03:20:28 -0300

    Create 2762.js
    Saída 06

---
+++ beginner/2762.js
@@ -0,0 +1,37 @@
+const { readFileSync } = require("fs")
+const input = readFileSync("/dev/stdin", "utf8").split("\n").shift()
+
+/** @typedef { number | bigint | string } anyNumberType */
+
+const Long = {
+	/**
+	 * @param { anyNumberType } numA
+	 * @param { anyNumberType } numB
+	 */
+	min: (numA, numB) => {
+		numA = BigInt(numA)
+		numB = BigInt(numB)
+		return numA <= numB ? numA : numB
+	}
+}
+
+const DEFFAULT_VALID_MAX_VALUE_NUM = Math.pow(2, 31) - 1
+
+/** @param { anyNumberType } originalNumber */
+
+function revertNumParts(originalNumber) {
+	return `${originalNumber}`
+		.split('.')
+		.slice(0, 2)
+		.map(part => part.replace(/^[0]+/, '') || '0')
+		.map(part => Long.min(part, DEFFAULT_VALID_MAX_VALUE_NUM))
+		.reverse()
+		.join('.')
+}
+
+function main() {
+	const reversedNums = revertNumParts(input)
+	console.log(`${reversedNums}`)
+}
+
+main()
