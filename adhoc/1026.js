07e6168fffea9197cb8660f52a443190423fa4bc
David Wesley <david.wesley755@outlook.com>
2021-12-08 15:51:11 -0300

    Create 1026.js

---
+++ adhoc/1026.js
@@ -0,0 +1,34 @@
+const { readFileSync } = require("fs")
+const input = readFileSync("/dev/stdin", "utf8").split(/\s+/g)
+
+/** @typedef { number | bigint | string } anyNumberType */
+
+const Long = {
+	/**
+	 * @param { anyNumberType } numA
+	 * @param { anyNumberType } numB
+	*/
+	xor: (numA, numB, bits = 32) => {
+		numA = BigInt.asUintN(bits, BigInt(numA))
+		numB = BigInt.asUintN(bits, BigInt(numB))
+
+		return numA ^ numB
+	}
+}
+
+function main() {
+	const output = []
+
+	for (let index = 0; index < input.length; index += 2) {
+		const v1 = input[index]
+		const v2 = input[index + 1]
+
+		if (v1 == '' || v2 == '') break
+
+		output.push(Long.xor(v1, v2))
+	}
+
+	console.log(`${output.join('\n')}`)
+}
+
+main()
