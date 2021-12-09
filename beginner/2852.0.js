89a6f905275c6e451ce7c6d1461f41036bd73e76
David Wesley <david.wesley755@outlook.com>
2021-12-09 16:16:34 -0300

    Add: 2852 file
     VigenèreCipher

---
+++ beginner/2852.0.js
@@ -0,0 +1,55 @@
+const { readFileSync } = require("fs")
+const [key, numLines, ...lines] = readFileSync("/dev/stdin", "utf8").split("\n")
+
+const ALPHABET = `ABCDEFGHIJKLMNOPQRSTUVWXYZ`.toUpperCase()
+
+function VigenèreCipher(private_key) {
+	const PRIVATE_KEY = String(private_key ?? '')
+
+	function createTable() {
+		const cypherTable = {}
+
+		for (let row = 0; row < PRIVATE_KEY.length; row++) {
+			cypherTable[PRIVATE_KEY.charAt(row)] = {}
+
+			for (let col = 0; col < PRIVATE_KEY.length; col++)
+				cypherTable[PRIVATE_KEY.charAt(row)][PRIVATE_KEY.charAt(col)] = PRIVATE_KEY.charAt((row + col) % PRIVATE_KEY.length)
+		}
+
+		return cypherTable
+	}
+
+	const CIPHER = createTable()
+
+	function encryptText(key, text) {
+		let index = 0
+
+		const encrypted = text.replace(
+			/\b((?![aeiou])[a-z][a-z]+)\b/gi,
+			(match) => {
+				return Array
+					.from(match, (char) => CIPHER[key.charAt(index++ % key.length).toUpperCase()][char.toUpperCase()] || char)
+					.join('')
+					.toLowerCase()
+			})
+
+		return encrypted
+	}
+
+	return {
+		cipher: CIPHER,
+		encrypt: encryptText
+	}
+}
+
+const { encrypt } = VigenèreCipher(ALPHABET)
+
+function main() {
+	const responses = lines
+		.slice(0, +numLines)
+		.map(line => encrypt(key, line))
+
+	console.log(`${responses.join("\n")}`)
+}
+
+main()
---
+++ beginner/2852.1.js
@@ -0,0 +1,58 @@
+const { readFileSync } = require("fs")
+const [key, numLines, ...lines] = readFileSync("/dev/stdin", "utf8").split("\n")
+
+const ALPHABET = `ABCDEFGHIJKLMNOPQRSTUVWXYZ`.toUpperCase()
+
+class VigenèreCipher {
+	#CIPHER_TABLE
+	#PRIVATE_KEY
+
+	constructor(private_key) {
+		this.#PRIVATE_KEY = String(private_key ?? '')
+		this.#CIPHER_TABLE = this.#createTable()
+	}
+
+	#createTable() {
+		const cypherTable = {}
+
+		for (let row = 0; row < this.#PRIVATE_KEY.length; row++) {
+			cypherTable[this.#PRIVATE_KEY.charAt(row)] = {}
+
+			for (let col = 0; col < this.#PRIVATE_KEY.length; col++)
+				cypherTable[this.#PRIVATE_KEY.charAt(row)][this.#PRIVATE_KEY.charAt(col)] = this.#PRIVATE_KEY.charAt((row + col) % this.#PRIVATE_KEY.length)
+		}
+
+		return cypherTable
+	}
+
+	encryptText(key, text, from = 0) {
+		return Array
+			.from(text, (char) => this.#CIPHER_TABLE[key.charAt(from++ % key.length).toUpperCase()][char.toUpperCase()] || char)
+			.join('')
+			.toLowerCase()
+	}
+}
+
+const vigenèreCipherInstance = new VigenèreCipher(ALPHABET)
+
+function main() {
+	const textList = lines.slice(0, +numLines)
+	const responses = new Array(+numLines)
+
+	for (const index in textList) {
+		const text = textList[index]
+
+		let from = 0
+
+		responses[index] = text.replace(/\b((?![aeiou])[a-z][a-z]+)\b/gi, (match) => {
+			const encryptedWord = vigenèreCipherInstance.encryptText(key, match, from)
+			from += encryptedWord.length
+
+			return encryptedWord
+		})
+	}
+
+	console.log(`${responses.join("\n")}`)
+}
+
+main()
