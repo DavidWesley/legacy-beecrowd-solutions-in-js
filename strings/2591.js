f0c3f56c3cb3ac185cc9bcb6d1cfeefa19ffa90c
David Wesley <david.wesley755@outlook.com>
2021-12-15 22:46:56 -0300

    Create 2591.js
    Problema do HameKameKa

---
+++ strings/2591.js
@@ -0,0 +1,22 @@
+"use strict"
+
+const { readFileSync } = require("fs")
+const [numCases, ...cases] = readFileSync("../dev/stdin", "utf8").split('\n')
+
+function ka(word) {
+	const times = word
+		.match(/a+/g)
+		.slice(0, 2)
+		.map(m => m.length)
+		.reduce((prod, value) => prod * value, 1)
+
+	return `k${'a'.repeat(times)}`
+}
+
+
+function main() {
+	const responses = cases.slice(0, +numCases).map(ka)
+	console.log(`${responses.join('\n')}`)
+}
+
+main()
