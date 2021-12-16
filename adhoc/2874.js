8d24ff43cf8f349519e2214ed3b9cead0bfcec9d
David Wesley <david.wesley755@outlook.com>
2021-12-15 22:36:22 -0300

    Update 2874.js

--- adhoc/2874.js
+++ adhoc/2874.js
@@ -1,29 +1,22 @@
 const { readFileSync } = require("fs")
 const input = readFileSync("/dev/stdin", "utf8").split('\n')
 
-/** @typedef {string | number} strNumType */
-/** @param {strNumType | (strNumType)[]} binariesList*/
+const BinFormmater = new Intl.NumberFormat('en-US', {
+	useGrouping: false,
+	minimumIntegerDigits: 8,
+	maximumFractionDigits: 0,
+})
 
-function binaryToASCII(binariesList) {
-	return String(binariesList)
-		.split(/\s|\,/)
-		.map((bin) => String.fromCharCode(Number.parseInt(bin, 2)))
-		.join('')
+while (input.length > 0) {
+	const line = input.shift()
+	if (line == '') break
+
+	const quantities = Number.parseInt(line, 10)
+
+	const binaries = input
+		.splice(0, quantities)
+		.map(BinFormmater.format)
+		.map(bin => Number.parseInt(bin, 2))
+
+	console.log(String.fromCharCode(...binaries))
 }
-
-function main() {
-	const responses = []
-
-	while (input.length > 0) {
-		const quantities = Number.parseInt(input.shift(), 10)
-		const binaries = input.splice(0, quantities)
-
-		if (Number.isNaN(quantities)) break
-
-		responses.push(binaryToASCII(binaries))
-	}
-
-	console.log(`${responses.join('\n')}`)
-}
-
-main()
