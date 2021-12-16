2e05ab2367af31cd29ebcf10632b1846b9ddfe40
David Wesley <david.wesley755@outlook.com>
2021-12-15 22:47:09 -0300

    Create 2108.js

---
+++ strings/2108.js
@@ -0,0 +1,24 @@
+const { readFileSync } = require("fs")
+const input = readFileSync("/dev/stdin", "utf8").split("\n")
+
+function main() {
+	let longestWord = ''
+	const responses = []
+
+	for (const line of input) {
+		if (line == '0') break
+
+		const formattedLine = line.split(' ').map((word) => {
+			let len = word.length
+			if (len >= longestWord.length) longestWord = word
+			return len
+		}).join('-')
+
+		responses.push(formattedLine)
+	}
+
+	console.log(`${responses.join('\n')}`)
+	console.log(`\nThe biggest word: ${longestWord}`)
+}
+
+main()
