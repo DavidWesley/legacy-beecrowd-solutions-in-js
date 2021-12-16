1695c6323747f34fe2176512f63395b8bdf2c63f
David Wesley <david.wesley755@outlook.com>
2021-12-15 22:36:52 -0300

    Create 2682.js

---
+++ adhoc/2682.js
@@ -0,0 +1,9 @@
+"use strict"
+
+const { readFileSync } = require("fs")
+const input = readFileSync("/dev/stdin", "utf8").split('\n').map(line => Number.parseInt(line, 10))
+
+input.pop() // Remove EOF
+
+const error = input.find((value, index, arr) => value > arr[index + 1]) || input[input.length - 1]
+console.log(`${error + 1}`)
