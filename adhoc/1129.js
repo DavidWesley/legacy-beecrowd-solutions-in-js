3fe59b29d997deaa03b921d90fa0aa477966edba
David Wesley <david.wesley755@outlook.com>
2021-12-16 14:28:09 -0300

    Create 1129.js Leitura Ótica

---
+++ adhoc/1129.js
@@ -0,0 +1,28 @@
+const { readFileSync } = require("fs")
+const input = readFileSync("/dev/stdin", "utf8").split('\n')
+
+function filledOrEmpty(shade) {
+	const inRange = ((value) => (min, max = value) => value === Math.min(Math.max(min, value), max))(+shade)
+
+	if (inRange(0, 127)) return 'B'
+	else if (inRange(128, 255)) return 'W'
+	else return ''
+}
+
+const output = []
+const AlternativesEnum = ['A', 'B', 'C', 'D', 'E']
+
+while (input.length > 0) {
+	const numQuestions = Number.parseInt(input.shift(), 10)
+
+	if (numQuestions == 0) break
+
+	const questions = input.splice(0, numQuestions).map(quest => quest.split(' ').map(filledOrEmpty))
+
+	for (const quest of questions) {
+		if (quest.filter(alt => alt == 'B').length != 1) output.push('*')
+		else output.push(AlternativesEnum[quest.indexOf('B')])
+	}
+}
+
+console.log(`${output.join('\n')}`)
