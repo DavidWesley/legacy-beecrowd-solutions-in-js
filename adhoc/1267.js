b3d70d609133abe7afe688fa204df8a759c20604
David Wesley <david.wesley755@outlook.com>
2021-12-16 23:26:23 -0300

    Create 1267.js Problema dos alumini

---
+++ adhoc/1267.js
@@ -0,0 +1,25 @@
+const { readFileSync } = require("fs")
+const input = readFileSync("/dev/stdin", "utf8").split('\n').map(line => line.split(' '))
+
+const output = []
+
+while (input.length > 0) {
+	const [A, D] = input.shift()
+
+	if (+A == 0 || +D == 0) break
+	if (isNaN(+A) || isNaN(+D)) break
+
+	const aluminisList = new Array(+A).fill(true)
+	const dinnersList = input.splice(0, +D).map(dinner => dinner.slice(0, +A))
+
+	for (const dinner of dinnersList) {
+		for (let index = 0; index < dinner.length; index++) {
+			if (aluminisList[index] == false) continue
+			else if (dinner[index] == '0') aluminisList[index] = false
+		}
+	}
+
+	output.push(aluminisList.includes(true) ? "yes" : "no")
+}
+
+console.log(`${output.join('\n')}`)
