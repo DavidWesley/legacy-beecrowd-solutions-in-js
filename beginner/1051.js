73c191c5f3838f3cded42420d27571f42a1084fb
David Wesley <david.wesley755@outlook.com>
2021-12-17 01:43:47 -0300

    income

---
+++ beginner/1051.js
@@ -0,0 +1,20 @@
+const { readFileSync } = require("fs")
+const [income] = readFileSync("/dev/stdin", "utf8").split('\n')
+
+const RangeConstructor = (value) => (min, max = Number.POSITIVE_INFINITY) => value === Math.min(Math.max(min, value), max)
+
+function incomeTax(income) {
+	if (isNaN(+income)) return 0
+	if (+income <= 0) return 0
+
+	const inRange = RangeConstructor(+income)
+
+	if (inRange(0, 2000)) return 0
+	else if (inRange(2000, 3000)) return 0.08 * (income - 2000) + incomeTax(2000)
+	else if (inRange(3000, 4500)) return 0.18 * (income - 3000) + incomeTax(3000)
+	else if (inRange(4500)) return 0.28 * (income - 4500) + incomeTax(4500)
+	else return 0
+}
+
+const tax = incomeTax(Number.parseFloat(income))
+console.log(tax > 0 ? `R$ ${tax.toFixed(2)}` : "Isento")
