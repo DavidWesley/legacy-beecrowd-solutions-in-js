f47b9313ce4a69ac46c37f826b792993d53d8fc5
David Wesley <david.wesley755@outlook.com>
2021-12-17 22:31:54 -0300

    Create 2428.js

---
+++ graphs/2428.js
@@ -0,0 +1,8 @@
+const { readFileSync } = require("fs")
+const [A1, A2, A3, A4] = readFileSync("/dev/stdin", "utf8")
+	.split(' ')
+	.slice(0, 4)
+	.map(area => Number.parseInt(area, 10))
+	.sort((areaA, areaB) => areaA - areaB)
+
+console.log((A1 * A4 == A2 * A3) ? 'S' : 'N')
