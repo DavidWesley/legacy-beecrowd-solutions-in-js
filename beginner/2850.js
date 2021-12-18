8610d216636e047564251724a2354bced9c3db24
David Wesley <david.wesley755@outlook.com>
2021-12-18 01:54:52 -0300

    Papagaio

---
+++ beginner/2850.js
@@ -0,0 +1,20 @@
+const { readFileSync } = require("fs")
+const input = readFileSync("/dev/stdin", "utf8").split('\n')
+
+const ParrotLegsStateEnum = {
+	"esquerda": "ingles",
+	"direita": "frances",
+	"nenhuma": "portugues",
+	"as duas": "caiu"
+}
+
+const output = []
+
+while (input.length > 0) {
+	const state = input.shift()
+	if (state == "") break // EOFile Condition
+
+	output.push(ParrotLegsStateEnum[state])
+}
+
+console.log(`${output.join('\n')}`)
