ffca99b7cd19187b9bbfb1c6881f474b74de487c
David Wesley <david.wesley755@outlook.com>
2021-12-18 20:44:19 -0300

    Create 1140.js

---
+++ adhoc/1140.js
@@ -0,0 +1,19 @@
+const { readFileSync } = require("fs")
+const input = readFileSync("/dev/stdin", "utf8").split('\n')
+
+function isTautogram(verse = '') {
+	const initials = (verse.match(/\b\w/g) ?? []).map((char) => char.toUpperCase())
+	return new Set(initials).size === 1
+}
+
+function main() {
+	const responses = []
+
+	for (const line of input)
+		if (line === '*') break
+		else responses.push(isTautogram(line) ? 'Y' : 'N')
+
+	console.log(`${responses.join('\n')}`)
+}
+
+main()
