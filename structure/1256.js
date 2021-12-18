9ea9d45e446bcf568d06fa1d7ccf4bc6cb75327c
David Wesley <david.wesley755@outlook.com>
2021-12-18 20:43:05 -0300

    Refactoring 1256.js

--- structure/1256.js
+++ structure/1256.js
@@ -1,64 +1,49 @@
-const { readFileSync } = require('fs')
-const inputs = readFileSync('/dev/stdin', 'utf8').split('\n')
+const { readFileSync } = require("fs")
+const [[numCases], ...cases] = readFileSync("/dev/stdin", "utf8")
+	.split("\n")
+	.map((line) => line.split(" ").map(Number))
 
-const responses = []
-const [numTestCases, ...cases] = inputs
+class FixedHashTable {
+	#mod
+	#hashTable
 
-for (let index = 0; cases.length > 0; index++) {
-    if (index === Number(numTestCases)) break
+	#hash(value) { return value % this.#mod }
 
-    const [roles, keys] = cases.splice(0, 2)
-    const hashTable = createHashTable([roles, keys])
+	constructor(size) {
+		this.#mod = size
+		this.#hashTable = new Map()
+		for (let index = 0; index < size; index++) this.#hashTable.set(index, new Array())
+	}
 
-    const ordenedhashTable = organizehashTable(hashTable) // Desnecessary Function
-    const formatedHashTable = formathashTableToPrint(ordenedhashTable)
-    responses.push(`${formatedHashTable}`)
+	insert([...values]) {
+		for (const value of values) {
+			const key = this.#hash(value)
+			this.#hashTable.get(key).push(value)
+		}
+
+		return this
+	}
+
+	print() {
+		const print = Array.from(this.#hashTable.entries(), ([key, values]) => [key, ...values, '\\'].join(" -> "))
+		return print.join("\n")
+	}
 }
 
-function createHashTable([roles, allKeys]) {
-    const [mod] = roles.split(' ').map(Number)
-    const keys = allKeys.split(' ').map(Number)
+function main() {
+	const responses = []
 
-    const objKeys = createKeysObjetctValues(mod, [])
+	for (let index = 0; cases.length > 0; index++) {
+		if (index === numCases) break
 
-    for (const key of keys) {
-        const disp = dispersion(key, mod)
-        const thisArr = objKeys[disp]
+		const [[baseAdressesNum, keysNum], keysList] = cases.splice(0, 2)
+		const hashTable = new FixedHashTable(baseAdressesNum).insert(keysList.slice(0, keysNum))
+		const formattedHashTable = hashTable.print()
 
-        objKeys[disp] = [...thisArr, key]
-    }
+		responses.push(`${formattedHashTable}`)
+	}
 
-    return objKeys
+	console.log(`${responses.join('\n\n')}`)
 }
 
-function createKeysObjetctValues(size, defaultStartValue = null, frozen = false) {
-    const arr = Array(size).fill(defaultStartValue)
-    const obj = Object.fromEntries(Object.entries(arr))
-
-    return frozen ? Object.freeze(obj) : obj
-}
-
-function dispersion(keyValue, modulus) {
-    return keyValue % modulus
-}
-
-function organizehashTable(hashTable) {
-    // for (const dispersionIndex in hashTable) {
-    //     const thisProp = hashTable[dispersionIndex].sort((a, b) => a - b)
-    //     hashTable[dispersionIndex] = [...thisProp]
-    // }
-
-    return hashTable
-}
-
-function formathashTableToPrint(hashTable) {
-    const print = []
-    for (const [dispersionIndex, values] of Object.entries(hashTable)) {
-        const message = [dispersionIndex, ...values]
-        print.push(message.join(" -> ").concat(" -> \\"))
-    }
-
-    return print.join("\n")
-}
-
-console.log(responses.join('\n\n'))
+main()
