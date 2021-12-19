87e73edbbe05589d6dc21441beb251b8428ea8b0
David Wesley <david.wesley755@outlook.com>
2021-12-19 00:44:54 -0300

    Add: 2653.js - Dijkstra

---
+++ beginner/2653.js
@@ -0,0 +1,18 @@
+const { readFileSync } = require("fs")
+const input = readFileSync("/dev/stdin", "utf8").split('\n')
+
+const countUniqueElements = ([...elements]) => new Set(elements).size
+const Dijkstra = ([...jewelriesList]) => countUniqueElements(jewelriesList)
+
+
+function main() {
+	const jewelriesList = []
+
+	for (const jewelry of input)
+		if (jewelry == '') break // EOFile Condition
+		else jewelriesList.push(jewelry)
+
+	console.log(`${Dijkstra(jewelriesList)}`)
+}
+
+main()
