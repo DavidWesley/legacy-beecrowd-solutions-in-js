0e1604565066a4f15ca7751077218f926d3e479e
David Wesley <david.wesley755@outlook.com>
2021-12-25 22:10:31 -0300

    ChristmasMessagesFromCountries

---
+++ structure/1763.js
@@ -0,0 +1,44 @@
+const { readFileSync } = require("fs")
+const input = readFileSync("/dev/stdin", "utf8").split('\n')
+
+const ChristmasMessagesFromCountries = {
+	"brasil": "Feliz Natal!",
+	"alemanha": "Frohliche Weihnachten!",
+	"austria": "Frohe Weihnacht!",
+	"coreia": "Chuk Sung Tan!",
+	"espanha": "Feliz Navidad!",
+	"grecia": "Kala Christougena!",
+	"estados-unidos": "Merry Christmas!",
+	"inglaterra": "Merry Christmas!",
+	"australia": "Merry Christmas!",
+	"portugal": "Feliz Natal!",
+	"suecia": "God Jul!",
+	"turquia": "Mutlu Noeller",
+	"argentina": "Feliz Navidad!",
+	"chile": "Feliz Navidad!",
+	"mexico": "Feliz Navidad!",
+	"antardida": "Merry Christmas!",
+	"canada": "Merry Christmas!",
+	"irlanda": "Nollaig Shona Dhuit!",
+	"belgica": "Zalig Kerstfeest!",
+	"italia": "Buon Natale!",
+	"libia": "Buon Natale!",
+	"siria": "Milad Mubarak!",
+	"marrocos": "Milad Mubarak!",
+	"japao": "Merii Kurisumasu!",
+}
+
+function main() {
+	const responses = []
+
+	for (const country of input) {
+		if (country === '') break // EOFile
+
+		const translatedMessage = ChristmasMessagesFromCountries[country] ?? "--- NOT FOUND ---"
+		responses.push(translatedMessage)
+	}
+
+	console.log(`${responses.join('\n')}`)
+}
+
+main()
