7909eff78be3a3988c8eb42eb6c4acc45f3c5520
David Wesley <david.wesley755@outlook.com>
2022-01-02 07:38:51 -0300

    COBOL - Economic Solution

---
+++ strings/2728.1.js
@@ -0,0 +1,30 @@
+// Economic Solution, but restricted
+
+const { readFileSync } = require("fs")
+const input = readFileSync("/dev/stdin", "utf8").split("\n")
+
+function gracehopper(words, separator = " ", targetWord) {
+	targetWord = targetWord.toUpperCase()
+
+	return words
+		.replace(/\b(\w)(\w*)(\w)\b/g, "$1$3")
+		.toUpperCase()
+		.split(separator)
+		.every((pairs, index) => pairs.includes(targetWord.charAt(index))) // Must be ordened!
+}
+
+function main() {
+	const responses = []
+	const TARGET = "COBOL"
+
+	for (const words of input) {
+		if (words === "") break // EOFile Condition Verification
+
+		const isMatched = gracehopper(words, "-", TARGET)
+		responses.push(isMatched ? "GRACE HOPPER" : "BUG")
+	}
+
+	console.log(`${responses.join("\n")}`)
+}
+
+main()
