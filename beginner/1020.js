507f0380378a39cc10504e25d2fc9f70e5a9a2d9
David Wesley <david.wesley755@outlook.com>
2022-01-03 06:18:22 -0300

    Fix(file): Finalmente recebendo acept no problema 1020

--- beginner/1020.js
+++ beginner/1020.js
@@ -1,20 +1,14 @@
 const { readFileSync } = require("fs")
-const input = readFileSync("/dev/stdin", "utf8").split('\n').shift()
-
-function formatDataByDaysNums(numDays = 0) {
-	const Y = Number.parseInt(String(numDays / 365), 10)
-	const M = Number.parseInt(String((numDays - (Y * 365)) / 30), 10)
-	const D = Number.parseInt(String((numDays % 365) % 30), 10)
-
-	return [D, M, Y]
-}
+const input = readFileSync("/dev/stdin", "utf8").split("\n").shift()
 
 function main() {
-	const ageInDays = Number.parseInt(input, 10)
-	const [days, months, years] = formatDataByDaysNums(ageInDays)
-	const print = `${years} ano(s) ${months} mes(s) ${days} dia(s)\n`
+	const N = Number.parseInt(input, 10)
 
-	console.log(print)
+	const Y = Math.floor(N / 365)
+	const M = Math.floor((N % 365) / 30)
+	const D = (N % 365) % 30
+
+	console.log("%d ano(s)\n%d mes(es)\n%d dia(s)", Y, M, D)
 }
 
 main()
