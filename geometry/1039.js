21ca3b8926d9575a365b1ea8336f07235d20fdb2
David Wesley <david.wesley755@outlook.com>
2022-01-03 22:17:25 -0300

    Refact: 1039

--- geometry/1039.js
+++ geometry/1039.js
@@ -1,10 +1,10 @@
 const { readFileSync } = require("fs")
 const coordenatesList = readFileSync("/dev/stdin", "utf8")
-	.split('\n')
-	.map(coords => coords.split(' ').map(value => Number.parseInt(value, 10)))
+	.split("\n")
+	.map((coords) => coords.split(" ").map((value) => Number.parseInt(value, 10)))
 
 function distanceBetween(x1 = 0, y1 = 0, x2 = x1, y2 = y1) {
-	return Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2))
+	return Math.hypot(x1 - x2, y1 - y2)
 }
 
 function main() {
@@ -12,10 +12,10 @@ function main() {
 
 	for (const [R1, X1, Y1, R2, X2, Y2] of coordenatesList) {
 		if (R1 < R2) responses.push("MORTO")
-		else responses.push(distanceBetween(X1, Y1, X2, Y2) > (R1 - R2) ? "MORTO" : "RICO")
+		else responses.push(distanceBetween(X1, Y1, X2, Y2) > R1 - R2 ? "MORTO" : "RICO")
 	}
 
-	console.log(responses.join('\n'))
+	console.log(responses.join("\n"))
 }
 
 main()
