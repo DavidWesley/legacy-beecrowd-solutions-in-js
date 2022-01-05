601eb4d103e537d144ea966aabc2ed24500887cc
David Wesley <david.wesley755@outlook.com>
2022-01-04 22:24:01 -0300

    Gerando Permutações Ordenadas Rapidamente

---
+++ strings/1401.js
@@ -0,0 +1,31 @@
+const { readFileSync } = require("fs")
+const [numLines, ...lines] = readFileSync("/dev/stdin", "utf8").split("\n")
+
+/** @returns {string[]} */
+
+function getPermutations(string, prefix = "") {
+	if (string.length <= 1) return [prefix + string]
+
+	return Array.from(string).reduce((result, char, index) => {
+		const reminder = string.slice(0, index) + string.slice(index + 1)
+		return result.concat(getPermutations(reminder, prefix + char))
+	}, [])
+}
+
+function main() {
+	const responses = lines.slice(0, +numLines).flatMap((line) => {
+		const sorted = [...new Set(getPermutations(line)).values()].sort((strA, strB) => {
+			if (strA < strB) return -1
+			if (strA > strB) return 1
+			return 0
+		})
+
+		sorted.push("")
+
+		return sorted
+	})
+
+	console.log(`${responses.join("\n")}`)
+}
+
+main()
