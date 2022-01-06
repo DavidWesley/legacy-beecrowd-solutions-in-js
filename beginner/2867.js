5d25f28190d550a6fefaf6d3d2ea27e4b99c8faa
David Wesley <david.wesley755@outlook.com>
2022-01-06 02:13:15 -0300

    Dígitos

---
+++ beginner/2867.js
@@ -0,0 +1,16 @@
+const { readFileSync } = require("fs")
+const [[numCases], ...lines] = readFileSync("/dev/stdin", "utf8")
+	.split("\n")
+	.map((line) => line.split(" "))
+
+function main() {
+	const responses = lines
+		.slice(0, +numCases)
+		.map(([N, M]) => {
+			return Math.floor(Number.parseInt(M, 10) * Math.log10(Number.parseInt(N, 10))) + 1
+		})
+
+	console.log(`${responses.join("\n")}`)
+}
+
+main()
