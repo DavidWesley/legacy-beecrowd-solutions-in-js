e29ec3c5d905d50eb5c0752833404439581952bc
David Wesley <david.wesley755@outlook.com>
2022-01-06 01:36:39 -0300

    TCC da Depressão Natalino

---
+++ beginner/2930.js
@@ -0,0 +1,16 @@
+const { readFileSync } = require("fs")
+const [E, D] = readFileSync("/dev/stdin", "utf8")
+	.split(" ")
+	.slice(0, 2)
+	.map((date) => Number.parseInt(date, 10))
+
+if (E > D) {
+	console.log("Eu odeio a professora!")
+} else if (E <= D - 3) {
+	console.log("Muito bem! Apresenta antes do Natal!")
+} else {
+	console.log("Parece o trabalho do meu filho!")
+
+	if (E + 2 < 24) console.log("TCC Apresentado!")
+	else console.log("Fail! Entao eh nataaaaal!")
+}
