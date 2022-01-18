f46ab0584fdb2e1360e3552781c187951759da93
David Wesley <david.wesley755@outlook.com>
2022-01-18 15:26:24 -0300

    Add: Andando no Tempo

---
+++ beginner/2235.js
@@ -0,0 +1,18 @@
+const { readFileSync } = require("fs")
+const [A, B, C] = readFileSync("/dev/stdin", "utf8")
+	.split(" ")
+	.slice(0, 3)
+	.map((value) => Number.parseInt(value, 10))
+
+if (
+	A + B == C
+	|| A + C == B
+	|| B + C == A
+	|| A == B
+	|| A == C
+	|| B == C
+) {
+	console.log("S")
+} else {
+	console.log("N")
+}
