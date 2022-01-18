9b6ce6263017e23981f8cf1a2e83745fc5acd2e3
David Wesley <david.wesley755@outlook.com>
2022-01-18 15:04:12 -0300

    Add: Ácido Ribonucleico Alienígena

---
+++ structure/1242.js
@@ -0,0 +1,34 @@
+const { readFileSync } = require("fs")
+const input = readFileSync("/dev/stdin", "utf8").split("\n")
+
+class RNAA {
+	static #BASES = ["BS", "SB", "FC", "CF"]
+	static #BASE_REGEX = RNAA.#BASES.map((base) => new RegExp(base, "g"))
+
+	static countconnections(RNAACode = "") {
+		let counter = 0
+
+		while (this.#BASE_REGEX.some((reg) => reg.test(RNAACode))) {
+			for (const reg of this.#BASE_REGEX) {
+				RNAACode = RNAACode.replace(reg, () => {
+					counter++
+					return ""
+				})
+			}
+		}
+
+		return counter
+	}
+}
+
+function main() {
+	const responses = new Array()
+
+	for (const RNA of input)
+		if (RNA == "") break
+		else responses.push(RNAA.countconnections(RNA))
+
+	console.log(`${responses.join("\n")}`)
+}
+
+main()
