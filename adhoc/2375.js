9e493da2874de5fa73c08569b0017ac5de3fdb63
David Wesley <david.wesley755@outlook.com>
2022-01-18 14:37:40 -0300

    Add: Sedex

---
+++ adhoc/2375.js
@@ -0,0 +1,9 @@
+const { readFileSync } = require("fs")
+const [sphereRadius, height, width, depth] = readFileSync("/dev/stdin", "utf8")
+	.split(/\s+/)
+	.slice(0, 4)
+	.map(Number.parseFloat)
+
+const isFits = sphereRadius <= Math.min(height, width, depth)
+
+console.log(isFits ? "S" : "N")
