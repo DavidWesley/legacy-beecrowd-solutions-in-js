b7f69959c03200c5aa37c59d635808df2d885af5
David Wesley <david.wesley755@outlook.com>
2022-01-18 15:10:54 -0300

    Add: Distância de Manhattan

---
+++ adhoc/2437.js
@@ -0,0 +1,10 @@
+const { readFileSync } = require("fs")
+const [Xm, Ym, Xr, Yr] = readFileSync("/dev/stdin", "utf8")
+	.split(" ")
+	.slice(0, 4)
+	.map((value) => Number.parseInt(value, 10))
+
+const Xdist = Math.abs(Xm - Xr)
+const Ydist = Math.abs(Ym - Yr)
+
+console.log(Xdist + Ydist)
