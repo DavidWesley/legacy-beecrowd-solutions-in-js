5cbb95b9e1d7c66201d0bf458bad785d560a4050
David Wesley <david.wesley755@outlook.com>
2022-01-18 15:34:32 -0300

    Add: Tempo de Jogo com Minutos

---
+++ beginner/1047.js
@@ -0,0 +1,14 @@
+const { readFileSync } = require("fs")
+const [H1, M1, H2, M2] = readFileSync("/dev/stdin", "utf8")
+	.split(" ")
+	.slice(0, 4)
+	.map((time) => Number.parseInt(time, 10))
+
+const h = ((24 + H2 - H1) % 24) * 60
+const m = M2 - M1
+const resolvedTimeInMinutes = (1440 + (h + m)) % 1440 || 1440
+
+const hours = Math.floor(resolvedTimeInMinutes / 60)
+const minutes = resolvedTimeInMinutes % 60
+
+console.log("O JOGO DUROU %d HORA(S) E %d MINUTO(S)", hours, minutes)
