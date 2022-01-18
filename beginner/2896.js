5b87d13e95f2a9b91bd7722c64d850ebb1d4ace5
David Wesley <david.wesley755@outlook.com>
2022-01-18 15:36:50 -0300

    Add: Aproveite a Oferta

---
+++ beginner/2896.js
@@ -0,0 +1,20 @@
+const { readFileSync } = require("fs")
+const [[numLines], ...lines] = readFileSync("/dev/stdin", "utf8")
+	.split("\n")
+	.map((line) => line.split(" ").map(value => Number.parseInt(value, 10)))
+
+
+function main() {
+	const responses = []
+
+	for (let i = 0; i < numLines; i++) {
+		const [purchasedNum, emptiesNum] = lines[i]
+		const bottlesLeftsOnHands = Math.trunc(purchasedNum / emptiesNum) + (purchasedNum) % (emptiesNum)
+
+		responses.push(bottlesLeftsOnHands)
+	}
+
+	console.log(`${responses.join("\n")}`)
+}
+
+main()
