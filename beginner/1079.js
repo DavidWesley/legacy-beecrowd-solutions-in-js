bd815aac231a629e11792babcdadac1bdfe47231
David Wesley <david.wesley755@outlook.com>
2022-01-18 15:37:39 -0300

    Add: Médias Ponderadas

---
+++ beginner/1079.js
@@ -0,0 +1,15 @@
+const { readFileSync } = require("fs")
+const [[numLines], ...lines] = readFileSync("/dev/stdin", "utf8")
+	.split("\n")
+	.map((line) => line.split(" ").map(Number.parseFloat))
+
+function weigthAverage(A = 0, B = 0, C = 0) {
+	return (2 * A + 3 * B + 5 * C) / 10
+}
+
+function main() {
+	const responses = lines.slice(0, +numLines).map(([a, b, c]) => weigthAverage(a, b, c).toFixed(1))
+	console.log(`${responses.join("\n")}`)
+}
+
+main()
