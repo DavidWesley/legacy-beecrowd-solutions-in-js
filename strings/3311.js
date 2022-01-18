6305a7a2b45b46f0408c3ea3c5de003bafda64d2
David Wesley <david.wesley755@outlook.com>
2022-01-18 15:00:31 -0300

    Add: Hudinho e o seu Trabalho

---
+++ strings/3311.js
@@ -0,0 +1,12 @@
+const { readFileSync } = require("fs")
+const [numLines, ...lines] = readFileSync("/dev/stdin", "utf8").split("\n")
+
+function main() {
+	const responses = lines
+		.slice(0, +numLines)
+		.sort((strA, strB) => strA.charAt(0).localeCompare(strB.charAt(0)))
+
+	console.log(`${responses.join("\n")}`)
+}
+
+main()
