82d760a79e7dc78985df8e3d745a0f35159cb9a8
David Wesley <david.wesley755@outlook.com>
2022-01-22 18:36:23 -0300

    Update: Removing unnecessary escape character in regex

--- structure/1673.js
+++ structure/1673.js
@@ -1,13 +1,14 @@
 const { readFileSync } = require("fs")
-const input = readFileSync("/dev/stdin", "utf8").split('\n')
+const input = readFileSync("/dev/stdin", "utf8")
+	.trimEnd() // Remove last line to pass in Debug
+	.split("\n")
 
-input.pop() // Remove last line to pass in Debug
 
 function RunLengthEncoding(text) {
 	return text
 		.replace(/(.)\1{1,8}/g, (match) => `[${match.length}#${match.charAt(0)}]`)
-		.replace(/(?<=^|\])(?<![2-9]\#)[a-zA-Z0-9 \-\.\,\:\;\?\!]+(?=(\[|$))/gm, (match) => `1${match.replace(/1/g, '11')}1`)
-		.replace(/[\[\]\#]/g, '')
+		.replace(/(?<=^|\])(?<![2-9]#)[a-zA-Z0-9 \-.,:;?!]+(?=(\[|$))/gm, (match) => `1${match.replace(/1/g, "11")}1`)
+		.replace(/[\[\]#]/g, "")
 }
 
 function main() {
