da3605b25831ebaac4ef44b8acd2232fe01c9611
David Wesley <david.wesley755@outlook.com>
2022-01-23 21:18:47 -0300

    Add: Soma de Propina

---
+++ strings/1898.js
@@ -0,0 +1,17 @@
+const { readFileSync } = require("fs")
+const [L1, L2] = readFileSync("/dev/stdin", "utf8")
+	.split("\n")
+	.slice(0, 2)
+	.map((L) => L
+		.replace(/[^\d.]/g, "")
+		.replace(/([.]\d{1,2})(\d*)/, "$1")
+	) // sanitizing each line
+
+
+const CPF = L1.substring(0, 11)
+
+const S1 = Number(L1.substr(11, 17))
+const S2 = Number(L2.substr(0, 17))
+
+console.log("cpf", CPF)
+console.log((S1 + S2).toFixed(2))
