3cb2c765f3ff516b7969f6e477d1cccea719d6fb
David Wesley <david.wesley755@outlook.com>
2022-01-24 00:59:20 -0300

    Add: Bem-vindos e Bem-vindas ao Inverno!

---
+++ beginner/1847.js
@@ -0,0 +1,41 @@
+const { readFileSync } = require("fs")
+
+const [A, B, C] = readFileSync("/dev/stdin", "utf8")
+	.split(" ")
+	.slice(0, 3)
+	.map((value) => Number.parseInt(value, 10))
+
+function main() {
+
+	if (
+		(A > B && B <= C) ||
+		(A > B && B > C && (B - C) < (A - B)) ||
+		(A < B && B < C && (C - B) >= (B - A)) ||
+		(A == B && B < C)
+	) { console.log(":)") }
+
+	if (
+		(A < B && B >= C) ||
+		(A < B && B < C && (C - B) < (B - A)) ||
+		(A > B && B > C && (B - C) >= (A - B)) ||
+		(A == B && B >= C)
+	) { console.log(":(") }
+
+	///////////////////////////////////////////////////////////////////
+
+	// if (A > B && B <= C) console.log(":)")
+
+	// else if (A < B && B >= C) console.log(":(")
+	// else if (A < B && B < C && (C - B) < (B - A)) console.log(":(")
+
+	// else if (A > B && B > C && (B - C) < (A - B)) console.log(":)")
+	// else if (A < B && B < C && (C - B) >= (B - A)) console.log(":)")
+	// else if (A > B && B > C && (B - C) >= (A - B)) console.log(":(")
+
+	// else if (A == B && B < C) console.log(":)")
+	// else if (A == B && B >= C) console.log(":(")
+
+	///////////////////////////////////////////////////////////////////
+}
+
+main()
