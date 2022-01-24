ed5216538248945d49bbde71b7bb9dcb42941e50
David Wesley <david.wesley755@outlook.com>
2022-01-24 19:29:58 -0300

    Update: Ho Ho Ho Diminuindo a complexidade do problema

--- beginner/1759.js
+++ beginner/1759.js
@@ -1,11 +1,11 @@
 const { readFileSync } = require("fs")
-const input = readFileSync("/dev/stdin", "utf8").split('\n')
+const input = readFileSync("/dev/stdin", "utf8").split("\n")
 
 function main() {
 	const repeat = Number.parseInt(input[0])
-	const HOs = ` Ho`.repeat(repeat - 1)
+	const HOs = "Ho ".repeat(repeat).trimEnd()
 
-	console.log(`Ho${HOs}!`)
+	console.log("%s!", HOs)
 }
 
 main()
