3e95424ae4390bd728b1c32ebab99de68fc31b2b
David Wesley <david.wesley755@outlook.com>
2022-02-21 19:39:36 -0300

    Add: A Mensagem de Will

---
+++ beginner/2523.js
@@ -0,0 +1,25 @@
+const { readFileSync } = require("fs")
+const input = readFileSync("/dev/stdin", "utf8").split("\n")
+
+
+function main() {
+	const responses = []
+
+	for (let index = 0; index < input.length; index += 3) {
+		if (input[index] == "") break // EOFile Condition
+
+		const searchableText = input[index]
+
+		const chars = input[index + 2]
+			.split(" ")
+			.slice(0, Number.parseInt(input[index + 1]))
+			.map((q) => searchableText[+q - 1])
+			.join("")
+
+		responses.push(chars)
+	}
+
+	console.log(responses.join("\n"))
+}
+
+main()
