7bb1213f8bfb3dfc2aa29bf0606fd761a2e2b1bc
David Wesley <david.wesley755@outlook.com>
2022-04-10 04:15:12 -0300

    Improviments, much faster code

--- strings/1873.js
+++ strings/1873.js
@@ -3,44 +3,94 @@ const [[numCases], ...lines] = readFileSync("/dev/stdin", "utf8")
 	.split("\n")
 	.map((line) => line.split(" "))
 
-class Rock_Paper_Scissors_Lizard_Spock {
-	static #players = ["tesoura", "lagarto", "papel", "pedra", "spock"]
-	static #RULES = {
-		tesoura: ["lagarto", "papel"],
-		lagarto: ["spock", "papel"],
-		papel: ["spock", "pedra"],
-		pedra: ["tesoura", "lagarto"],
-		spock: ["tesoura", "pedra"]
+/**
+ * @typedef {{name: string, choice: string}} playerType
+ */
+
+class Referee {
+	#rules = {}
+	constructor() { }
+
+	validate(choice = "") {
+		return Reflect.has(this.#rules, choice)
+	}
+	get choices() {
+		return Object.keys(this.#rules)
 	}
 
-	static get players() {
-		return this.#players
+	/**
+	 * @param {string} winner
+	 * @param {string} loser
+	 */
+	learn(winner, loser) {
+		if (!this.validate(winner)) {
+			this.#rules[winner] = {}
+		}
+		this.#rules[winner][loser] = 1
+
+		return this
 	}
 
-	static winner(player1 = "", player2 = "") {
-		player1 = player1.toLowerCase()
-		player2 = player2.toLowerCase()
+	/**
+	 * @param {playerType} player1
+	 * @param {playerType} player2
+	 */
+	judge(player1, player2) {
+		return player1.choice === player2.choice
+			? null
+			: this.#rules[player1.choice][player2.choice] === 1
+				? player1
+				: player2
+	}
+}
 
-		const staricClasReference = Rock_Paper_Scissors_Lizard_Spock
-		const playersReference = staricClasReference.#players
+const Game = class Rock_Paper_Scissors_Lizard_Spock {
+	static #RULES = new Referee()
+		.learn("tesoura", "lagarto").learn("tesoura", "papel")
+		.learn("lagarto", "spock").learn("lagarto", "papel")
+		.learn("papel", "spock").learn("papel", "pedra")
+		.learn("pedra", "tesoura").learn("pedra", "lagarto")
+		.learn("spock", "tesoura").learn("spock", "pedra")
 
-		if ((playersReference.includes(player1) && playersReference.includes(player2)) == false)
-			throw new Error("Wrong Player Name!")
+	static get choices() {
+		return this.#RULES.choices
+	}
 
-		if (staricClasReference.#RULES[player1].includes(player2)) return player1
-		if (staricClasReference.#RULES[player2].includes(player1)) return player2
-		return "empate"
+	/**
+	 * @param {playerType} player1
+	 * @param {playerType} player2
+	 */
+	static winner(player1, player2) {
+		const staticClassRef = Rock_Paper_Scissors_Lizard_Spock
+
+		if (
+			(
+				staticClassRef.#RULES.validate(player1.choice) &&
+				staticClassRef.#RULES.validate(player2.choice)
+			) == false
+		)
+			throw new Error("Wrong Player's Choice!")
+
+		return staticClassRef.#RULES.judge(player1, player2)
 	}
 }
 
 function main() {
-	const responses = lines.slice(0, +numCases).map(([p1, p2]) => {
-		const result = Rock_Paper_Scissors_Lizard_Spock.winner(p1, p2)
+	const nameA = "rajesh"
+	const nameB = "sheldon"
 
-		if (result == p1) return "rajesh"
-		if (result == p2) return "sheldon"
-		return "empate"
-	})
+	const responses = lines
+		.slice(0, Number.parseInt(numCases, 10))
+		.map(([c1, c2]) => {
+			const /** @type {playerType}*/ p1 = { name: nameA, choice: c1 }
+			const /** @type {playerType}*/ p2 = { name: nameB, choice: c2 }
+
+			const result = Game.winner(p1, p2)
+
+			if (result === null) return "empate"
+			else if (result.choice === c1) return result.name
+			else if (result.choice === c2) return result.name
+		})
 
 	console.log(responses.join("\n"))
 }
