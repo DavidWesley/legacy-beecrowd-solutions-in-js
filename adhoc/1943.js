46d88c78b8c42158f387edf3b665a5691cc3e44b
David Wesley <david.wesley755@outlook.com>
2022-04-17 15:05:51 -0300

    linting 1943.js file

--- adhoc/1943.js
+++ adhoc/1943.js
@@ -1,12 +1,11 @@
-import { readFileSync } from "node:fs"
-const [position] = readFileSync("/dev/stdin", "utf8").split('\n')
+const { readFileSync } = require("fs")
+const [position] = readFileSync("/dev/stdin", "utf8").split("\n")
 
 const TOPS = [1, 3, 5, 10, 25, 50, 100]
 
 function main() {
 	const topList = TOPS.find(top => top >= +position)
-
-	console.log(`Top ${topList}`)
+	console.log("Top", topList)
 }
 
 main()
