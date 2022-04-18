0e12520f3e9ef9ec1fa53e673594071563277b0d
David Wesley <david.wesley755@outlook.com>
2022-04-17 21:08:47 -0300

    Refact: over engineering code to keep minimum SOLID principle

--- strings/1873.js
+++ strings/1873.js
@@ -23,24 +23,30 @@ class Referee {
 	 * @param {string} loser
 	 */
 	learn(winner, loser) {
-		if (!this.validate(winner)) {
-			this.#rules[winner] = {}
-		}
+		if (this.validate(loser) == false) this.#rules[loser] = {}
+		if (this.validate(winner) == false) this.#rules[winner] = {}
+
 		this.#rules[winner][loser] = 1
 
 		return this
 	}
 
 	/**
-	 * @param {playerType} player1
-	 * @param {playerType} player2
+	 * @param {playerType} playerA
+	 * @param {playerType} playerB
+	 *
+	 * @return {{ status: "draw" | "win", data: { winner: playerType, loser: playerType } }}
 	 */
-	judge(player1, player2) {
-		return player1.choice === player2.choice
-			? null
-			: this.#rules[player1.choice][player2.choice] === 1
-				? player1
-				: player2
+	judge(playerA, playerB) {
+		const status = playerA.choice === playerB.choice ? "draw" : "win"
+		const data = { winner: null, loser: null }
+
+		if (status === "win") {
+			data.winner = this.#rules[playerA.choice][playerB.choice] === 1 ? playerA : playerB
+			data.loser = data.winner === playerA ? playerB : playerA
+		}
+
+		return { status, data }
 	}
 }
 
@@ -57,21 +63,21 @@ const Game = class Rock_Paper_Scissors_Lizard_Spock {
 	}
 
 	/**
-	 * @param {playerType} player1
-	 * @param {playerType} player2
+	 * @param {playerType} playerA
+	 * @param {playerType} playerB
 	 */
-	static winner(player1, player2) {
+	static winner(playerA, playerB) {
 		const staticClassRef = Rock_Paper_Scissors_Lizard_Spock
 
 		if (
 			(
-				staticClassRef.#RULES.validate(player1.choice) &&
-				staticClassRef.#RULES.validate(player2.choice)
+				staticClassRef.#RULES.validate(playerA.choice) &&
+				staticClassRef.#RULES.validate(playerB.choice)
 			) == false
 		)
-			throw new Error("Wrong Player's Choice!")
+			throw new Error(`Wrong Player's Choice! > ${playerA.choice} or ${playerB.choice}`)
 
-		return staticClassRef.#RULES.judge(player1, player2)
+		return staticClassRef.#RULES.judge(playerA, playerB).data.winner
 	}
 }
 
@@ -81,15 +87,14 @@ function main() {
 
 	const responses = lines
 		.slice(0, Number.parseInt(numCases, 10))
-		.map(([c1, c2]) => {
-			const /** @type {playerType}*/ p1 = { name: nameA, choice: c1 }
-			const /** @type {playerType}*/ p2 = { name: nameB, choice: c2 }
+		.map(([choiceA, choiceB]) => {
+			const /** @type {playerType}*/ p1 = { name: nameA, choice: choiceA }
+			const /** @type {playerType}*/ p2 = { name: nameB, choice: choiceB }
 
-			const result = Game.winner(p1, p2)
+			const winner = Game.winner(p1, p2)
 
-			if (result === null) return "empate"
-			else if (result.choice === c1) return result.name
-			else if (result.choice === c2) return result.name
+			if (winner === null) return "empate"
+			else return winner.name
 		})
 
 	console.log(responses.join("\n"))
