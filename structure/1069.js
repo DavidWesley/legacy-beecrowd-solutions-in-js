3a88eb7d9a92c4438b9a1ea218a021e632e2b965
David Wesley <david.wesley755@outlook.com>
2022-06-05 01:38:35 -0300

    Melhorando, como foi com o 1068

--- structure/1069.js
+++ structure/1069.js
@@ -1,49 +1,58 @@
 const { readFileSync } = require("fs")
-const [numCases, ...cases] = readFileSync("/dev/stdin", "utf8").split("\n")
+const [numLines, ...lines] = readFileSync("/dev/stdin", "utf8").split("\n")
+
+class Stack {
+	#items = []
+
+	push(element) { this.#items.push(element) }
+	pop() { return this.#items.pop() }
+
+	toArray() { return this.#items }
+	isEmpty() { return this.size == 0 }
+
+	get size() { return this.#items.length }
+	get peek() { return this.#items.at(-1) }
+
+	static fromArray(arr = []) {
+		const stack = new Stack()
+		for (const item of arr) stack.push(item)
+		return stack
+	}
+}
 
 function main() {
-	const responses = cases.slice(0, +numCases).map((_, index) => {
-		const sign = [...cases[index]]
-		const foundedDiamonds = findDiamonds(sign)
-
-		return foundedDiamonds
-	})
+	const responses = lines
+		.slice(0, +numLines)
+		.map(countDiamondsInString)
 
 	console.log(responses.join("\n"))
 }
 
 main()
 
-/** @param {string[]} sign */
-
-function findDiamonds(sign = []) {
-	const signs = {
-		sign: sign,
-		stacks: {
-			LTS: [],
-			GTS: []
-		},
+function countDiamondsInString(cs = "") {
+	const details = {
 		codes: {
-			LTS: String.fromCharCode(60), // <
-			GTS: String.fromCharCode(62) // >
-		},
-		diamonds: {
-			counter: 0
+			comparator: {
+				LTS: String.fromCharCode(60), // <
+				GTS: String.fromCharCode(62) // >
+			}
 		}
 	}
 
-	signs.sign.forEach((symbol, index) => {
-		if (symbol === signs.codes.LTS)
-			signs.stacks.LTS.push(index)
-		else if (symbol === signs.codes.GTS) {
-			signs.stacks.GTS.push(index)
+	let diamonds = 0
 
-			if (signs.stacks.LTS.length > 0) {
-				signs.stacks.LTS.pop()
-				++signs.diamonds.counter
-			}
+	const comparatorStack = new Stack()
+	const comparatorsMatchesArray = Array.from(cs.match(/[<>]/g) || [])
+
+	for (const comparator of comparatorsMatchesArray) {
+		if (comparator === details.codes.comparator.LTS)
+			comparatorStack.push(comparator)
+		else if (comparator === details.codes.comparator.GTS && comparatorStack.isEmpty() === false) {
+			comparatorStack.pop()
+			diamonds += 1
 		}
-	})
+	}
 
-	return signs.diamonds.counter
+	return diamonds
 }
