681cd9588b7b553da995f7ca34f8a05e916577fc
David Wesley <david.wesley755@outlook.com>
2022-06-05 16:50:17 -0300

    Sequência Lógica Cleaning code to more simple reading

--- beginner/1144.js
+++ beginner/1144.js
@@ -1,34 +1,23 @@
 const { readFileSync } = require("fs")
-const input = Number.parseInt(readFileSync("/dev/stdin", "utf8").split("\n")[0])
 
-function chunkArray(myArray = [], chunkSize = myArray.length) {
-	const results = []
-	while (myArray.length > 0) results.push(myArray.splice(0, chunkSize))
-
-	return results
-}
-
-function modelsArrays(limit = 0) {
-	const results = []
-
-	for (let value = 1; value <= limit; value++) {
-		results.push(value, value ** 2, value ** 3)
-		results.push(value, value ** 2 + 1, value ** 3 + 1)
-	}
-
-	return results
-}
+const [size] = readFileSync("/dev/stdin", "utf8")
+	.split("\n", 1)
+	.map(Number.parseFloat)
 
 function main() {
-	const responses = []
 
-	const size = 3
-	const model = modelsArrays(input)
-	const chunksArr = chunkArray(model, size)
+	const output = []
 
-	for (const chunk of chunksArr) responses.push(chunk.join(" "))
+	for (let value = 1; value <= size; value++) {
+		output.push([value, value ** 2, value ** 3])
+		output.push([value, value ** 2 + 1, value ** 3 + 1])
+	}
 
-	console.log(responses.join("\n"))
+	console.log(
+		output
+			.map(chunk => chunk.join(" "))
+			.join("\n")
+	)
 }
 
 main()
