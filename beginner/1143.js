6964649714e073d5f98c25cc2370c06bc2b9b1b6
David Wesley <david.wesley755@outlook.com>
2022-06-05 16:54:29 -0300

    Quadrado e ao Cubo Cleaning code to more simple reading

--- beginner/1143.js
+++ beginner/1143.js
@@ -1,26 +1,22 @@
 const { readFileSync } = require("fs")
-const input = readFileSync("/dev/stdin", "utf8").split("\n").shift()
 
-function range(start, end = start, step = 1) {
-	const length = Math.floor((end - start + 1) / step) + (Math.sign(Math.min(start, end)) === -1 ? 1 : 0)
-	return Array.from({ length }, (_, i) => start + step * i)
-}
+const [size] = readFileSync("../dev/stdin", "utf8")
+	.split("\n", 1)
+	.map(Number.parseFloat)
 
 function main() {
-	const responses = []
-	const limit = Number.parseInt(input)
 
-	const baseSequence = range(1, limit)
-	const seqPowerToTwo = baseSequence.map((value) => Math.pow(value, 2))
-	const seqPowerToThree = baseSequence.map((value) => Math.pow(value, 3))
+	const output =
+		Array.from({ length: size }, (_, index) => {
+			const value = index + 1
+			return [value, value ** 2, value ** 3]
+		})
 
-	for (const index in baseSequence) {
-		responses.push(
-			`${baseSequence[index]} ${seqPowerToTwo[index]} ${seqPowerToThree[index]}`
-		)
-	}
-
-	console.log(responses.join("\n"))
+	console.log(
+		output
+			.map(chunk => chunk.join(" "))
+			.join("\n")
+	)
 }
 
 main()
