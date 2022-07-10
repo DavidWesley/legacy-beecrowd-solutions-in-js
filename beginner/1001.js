43d9ecf09e1f33a5fbadf0ecdff827424f066217
David Wesley <david.wesley755@outlook.com>
2022-07-10 19:47:15 -0300

    1001 - simple refactoring

--- beginner/1001.js
+++ beginner/1001.js
@@ -1,7 +1,6 @@
 const { readFileSync } = require("fs")
 const [x, y] = readFileSync("/dev/stdin", "utf8")
-	.split("\n")
-	.slice(0, 2)
+	.split("\n", 2)
 	.map(value => Number.parseInt(value, 10))
 
 const sum = x + y
