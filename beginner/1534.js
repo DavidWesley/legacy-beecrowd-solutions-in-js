13c9dff4b8eb21c586018e729654bc4c90ec4295
David Wesley <david.wesley755@outlook.com>
2022-07-21 01:15:08 -0300

    Refactoring: 1534.js -> Adding Matrix class

--- beginner/1534.js
+++ beginner/1534.js
@@ -1,26 +1,40 @@
-const { readFileSync } = require("fs")
-const input = readFileSync("/dev/stdin", "utf8").split("\n")
+const { readFileSync } = require("node:fs")
+
+const sizes = readFileSync("/dev/stdin", "utf8")
+	.split("\n")
+	.map(line => Number.parseInt(line, 10))
 
 /** @param {number} size */
-const createSquareModel = (size) => ({ lengths: { cols: size, rows: size } })
+function createSquareModel(size) {
+	return ({ lengths: { cols: size, rows: size } })
+}
 
-function createMatrixFromModel(model = { lengths: { rows: 0, cols: 0 } }, values = [], defaultReplecmentValue = "0") {
-	const { rows: rowsLenght, cols: colsLength } = model.lengths
+class Matrix {
 
-	const defaultRows = () => new Array(colsLength).fill(defaultReplecmentValue)
+	/**
+ * Create a matrix from a object model
+ * @param {ReturnType<typeof createSquareModel>} model
+ * @param {Array<any>} values
+ * @param {any} defaultReplacementValue
+ */
+	static createMatrixFromModel(model, values, defaultReplacementValue = null) {
+		const { rows, cols } = model.lengths
 
-	return Array.from({ length: rowsLenght }, () => {
-		return defaultRows().map((defaultValue) => values?.shift() ?? defaultValue)
-	})
+		return Array.from({ length: rows }, (_, rowIndex) => {
+			return Array.from({ length: cols }, (_, colIndex) => {
+				return values[(rows * rowIndex) + colIndex] ?? defaultReplacementValue
+			})
+		})
+	}
 }
 
 function main() {
 	const responses = []
-	const sizes = input.map(size => Number.parseInt(size))
 
-	sizes.forEach(size => {
+	for (const size of sizes) {
+		if (isNaN(size)) break // Possible EOFile Condition
 		const model = createSquareModel(size)
-		const matrix = createMatrixFromModel(model, [], "3")
+		const matrix = Matrix.createMatrixFromModel(model, [], "3")
 
 		for (let i = 0, u = size - 1; i < size; i++, u--) {
 			matrix[i][i] = "1"
@@ -28,7 +42,7 @@ function main() {
 
 			responses.push(matrix[i].join(""))
 		}
-	})
+	}
 
 	console.log(responses.join("\n"))
 }
