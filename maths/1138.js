79e4bb501711f9f2c7b04b17bfc61ba439d3ebe0
David Wesley <david.wesley755@outlook.com>
2022-07-23 20:39:43 -0300

    Refactor: 1138.js - Add DigitsCounter Class

--- maths/1138.js
+++ maths/1138.js
@@ -1,75 +1,79 @@
 const { readFileSync } = require("fs")
 
-const input = readFileSync("/dev/stdin", "utf8").split("\n").map(line => {
-	const [A, B] = line.split(" ")
-	return [Number.parseInt(A, 10), Number.parseInt(B, 10)]
-})
+const input = readFileSync("/dev/stdin", "utf8")
+	.split("\n")
+	.map(line => line.split(" ", 2).map(value => Number.parseInt(value, 10)))
 
-/**
- * @param {number} num
- * @param {number} digit
- * @returns {number}
- */
 
-function occurrences(num, digit) {
-	const K = Math.trunc(Math.log10(num))
+class DigitsCounter {
+	/**
+	 * A private object memory that contains all calculates list
+	 * to improve CPU performance
+	 */
+	static #memo = {}
 
-	if (K === 0) return num >= digit ? 1 : 0
+	/**
+	 * @param {number} num
+	 * @param {number} digit
+	 * @returns {number}
+	 */
+	static occurrences(num, digit) {
+		const K = Math.trunc(Math.log10(num))
 
-	const tenPowK = Math.pow(10, K)
-	const tenPowKMinus1 = tenPowK / 10
+		if (K === 0) return num >= digit ? 1 : 0
 
-	const A = Math.trunc(num / tenPowK)
-	const B = num % tenPowK
+		const tenPowK = Math.pow(10, K)
+		const tenPowKMinus1 = tenPowK / 10
 
-	if (A < digit) return (A * K) * tenPowKMinus1 + (B === 0 ? 0 : occurrences(B, digit))
-	else if (A === digit) return (A * K) * tenPowKMinus1 + 1 + (B === 0 ? 0 : occurrences(B, digit)) + B
-	else if (A > digit) return (A * K + 10) * tenPowKMinus1 + (B === 0 ? 0 : occurrences(B, digit))
+		const A = Math.trunc(num / tenPowK)
+		const B = num % tenPowK
 
-	return 0
+		if (A < digit) return (A * K) * tenPowKMinus1 + (B === 0 ? 0 : this.occurrences(B, digit))
+		else if (A === digit) return (A * K) * tenPowKMinus1 + 1 + (B === 0 ? 0 : this.occurrences(B, digit)) + B
+		else if (A > digit) return (A * K + 10) * tenPowKMinus1 + (B === 0 ? 0 : this.occurrences(B, digit))
+
+		return 0
+	}
+
+	/** @param {number} num */
+	static total(num) {
+		if (num < 10) return num + 1
+
+		const K = Math.trunc(Math.log10(num))
+		const tenPowK = Math.pow(10, K)
+
+		const A = Math.trunc(num / tenPowK)
+		const B = num % tenPowK
+
+		let sum = 0
+
+		for (let s = 1; s <= K; s++)
+			sum += 9 * s * Math.pow(10, s - 1)
+
+		return (K + 1) * (B + 1 + (A - 1) * tenPowK) + sum + 1
+	}
+
+	/**
+	 * @param {number} num
+	 * @returns {number[]}
+	 */
+	static occurrencesList(num) {
+		if (Reflect.has(DigitsCounter.#memo, num)) return DigitsCounter.#memo[num]
+
+		let sum = 0
+		const list = Array(10).fill(0)
+
+		for (let digit = 1; digit <= 9; digit++) {
+			list[digit] = this.occurrences(num, digit)
+			sum += list[digit]
+		}
+
+		// Tratamento especial para o ZERO:
+		list[0] = this.total(num) - sum
+		Reflect.set(DigitsCounter.#memo, num, list)
+		return list
+	}
 }
-
-/** @param {number} num */
-
-function totalDigits(num) {
-	if (num < 10) return num + 1
-
-	const K = Math.trunc(Math.log10(num))
-	const tenPowK = Math.pow(10, K)
-
-	const A = Math.trunc(num / tenPowK)
-	const B = num % tenPowK
-
-	let sum = 0
-
-	for (let s = 1; s <= K; s++)
-		sum += 9 * s * Math.pow(10, s - 1)
-
-	return (K + 1) * (B + 1 + (A - 1) * tenPowK) + sum + 1
-}
-
-/**
- * @param {number} num
- * @returns {number[]}
- */
-
-function countOccurrences(num) {
-	const counter = Array(10).fill(0)
-
-	for (let d = 1; d < 10; d++)
-		counter[d] = occurrences(num, d)
-
-	// Tratamento especial para o ZERO:
-
-	const dSum = counter.reduce((sum, value) => value + sum, 0)
-	counter[0] = totalDigits(num) - dSum
-
-	return counter
-}
-
-
-// OPTIMIZATION TEST
-const memo = {}
 
 function main() {
 	const responses = []
@@ -77,14 +81,12 @@ function main() {
 	for (const [A, B] of input) {
 		if (A === 0 && B === 0) break // EOFile Condition
 
-		const X = A !== 0 ? A - 1 : 0 // For Zero Value we need of this Special Tratament
-		const Y = B // Reatribuição desnecessária
+		// For Zero Value we need of this Special Tratament
+		const X = A !== 0 ? A - 1 : 0
+		const Y = B
 
-		const min = memo[X] || countOccurrences(X)
-		const max = memo[Y] || countOccurrences(Y)
-
-		if (!(X in memo)) memo[X] = min // Trying optimazing
-		if (!(Y in memo)) memo[Y] = max // Trying optimazing
+		const min = DigitsCounter.occurrencesList(X)
+		const max = DigitsCounter.occurrencesList(Y)
 
 		const result = Array.from({ length: 10 }, (_, i) => max[i] - min[i])
 
