8432dbde2d1b5ad3aa535962bca98ae29b821d99
David Wesley <david.wesley755@outlook.com>
2022-07-25 13:12:21 -0300

    Refactor: 2956.js Simplificando o código

--- maths/2956.js
+++ maths/2956.js
@@ -1,18 +1,10 @@
-const { readFileSync } = require("fs")
+const { readFileSync } = require("node:fs")
 
-const [base, height] = readFileSync("/dev/stdin", "utf8")
-	.split(" ")
-	.slice(0, 2)
+const [B, H] = readFileSync("/dev/stdin", "utf8")
+	.split(" ", 2)
 	.map(Number.parseFloat)
 
-const triangleArea = (B, H) => (B * H) / 2
 
-function main() {
-	const area = triangleArea(+base, +height)
-
-	console.log(
-		`Concluimos que, dado o limite da entrada, a resposta seria:  y = f(x) = ${area.toFixed(5)}.`
-	)
-}
-
-main()
+console.log(
+	"Concluimos que, dado o limite da entrada, a resposta seria:  y = f(x) = %s.", ((B * H) / 2).toFixed(5)
+)
