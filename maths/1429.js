93cab0f80830e76104be581012126a33c5054807
David Wesley <david.wesley755@outlook.com>
2022-07-26 07:11:07 -0300

    Refact: 1429 - Update Factorial

--- maths/1429.js
+++ maths/1429.js
@@ -1,41 +1,44 @@
 const { readFileSync } = require("fs")
 const input = readFileSync("/dev/stdin", "utf8").split("\n")
 
-function Factorial() {
-	const factorialValues = new Map([[0, 1n], [1, 1n]])
+class Factorial {
+	static #list = new Map([
+		[0, 1n], [1, 1n]
+	])
 
-	function fact(/** @type {number}*/num) {
-		let returnedValue = 1
-		for (let f = 2; f <= num; f++) returnedValue *= f
-		return BigInt(returnedValue)
+	/** @param {number} nth */
+	static get(nth) {
+		if (Factorial.#list.has(nth))
+			return Factorial.#list.get(nth)
+
+		let value = Factorial.#list.get(Factorial.#list.size - 1)
+
+		for (let f = Factorial.#list.size; f <= nth; f++) {
+			value *= BigInt(f)
+			Factorial.#list.set(f, value)
+		}
+
+		return Factorial.#list.get(nth)
 	}
-
-	function updateFactorialList(/** @type {number}*/num) {
-		if (!factorialValues.has(num)) factorialValues.set(num, fact(num))
-		return factorialValues
-	}
-
-	const factorial = (/** @type {number}*/num) => updateFactorialList(num).get(num)
-
-	return { factorial }
 }
 
 /** @param {number | string} acm*/
-/** @param {ReturnType<Factorial>} factListInstance*/
 
-function ACMToDec(acm, factListInstance) {
-	return [...`${acm}`].reverse().reduce((sum, decimal, index) => sum + (factListInstance.factorial(index + 1) * BigInt(decimal)), 0n)
+function ACMToDec(acm) {
+	return [...`${acm}`]
+		.reverse()
+		.reduce((sum, decimal, index) => sum + Factorial.get(index + 1) * BigInt(decimal), 0n)
 }
 
 function main() {
-	const stopAtIndex = input.indexOf("0")
-	const factorialListInstance = Factorial()
+	const output = []
 
-	const responses = input
-		.slice(0, stopAtIndex)
-		.map(acm => ACMToDec(acm, factorialListInstance))
+	for (const line of input) {
+		if (line == "0") break
+		else output.push(ACMToDec(line))
+	}
 
-	console.log(responses.join("\n"))
+	console.log(output.join("\n"))
 }
 
 main()
