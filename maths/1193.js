6cb22a1961e2e98b2e70e3f6206e9e5e71af91bb
David Wesley <david.wesley755@outlook.com>
2022-07-27 05:54:00 -0300

    Refact: 1193.js Tornando o código mais simples

--- maths/1193.js
+++ maths/1193.js
@@ -1,52 +1,45 @@
 const { readFileSync } = require("fs")
-const [[entriesLength], ...entries] = readFileSync("/dev/stdin", "utf8")
+const [[numLines], ...lines] = readFileSync("/dev/stdin", "utf8")
 	.split("\n")
-	.map(entry => entry.split(" "))
+	.map(line => line.split(" ", 2))
 
 
 const ConvertBase = (num) => ({
 	from: (baseFrom) => ({
-		to: (baseTo) => parseInt(num, baseFrom).toString(baseTo)
+		to: (baseTo) => Number.parseInt(num, baseFrom).toString(baseTo)
 	})
 })
 
-/** @type {[string, number][]} */
-const radixesEntries = [
-	// ['oct', 8],
-	["dec", 10],
-	["hex", 16],
-	["bin", 2]
-] // Ordenado dessa forma para sair como o Udebug aceita
+const radices = Object.freeze({
+	dec: 10,
+	hex: 16,
+	bin: 2
+})
 
-const radixes = Object.fromEntries(radixesEntries)
 
 function main() {
-	const responses = []
+	const output = []
 
-	const numbersEntries = entries.slice(0, +entriesLength)
+	for (let index = 0; index < Number.parseInt(numLines, 10); index++) {
+		output.push(`Case ${index + 1}:`)
 
-	for (const [index, [num, radixCode]] of numbersEntries.entries()) {
-
-		const currentArray = []
-
-		currentArray.push(`Case ${+index + 1}:`)
-
+		const [num, code] = lines[index]
 		const convertNum = ConvertBase(num)
-		const otherCodes = Object.keys(radixes).filter(code => code !== radixCode)
 
-		otherCodes.forEach(code => {
-			const baseFrom = radixes[radixCode]
-			const baseTo = radixes[code]
+		for (const radix in radices) {
+			if (code === radix) continue
 
-			currentArray.push(`${convertNum.from(baseFrom).to(baseTo)} ${code}`)
-		})
+			const baseFrom = radices[code]
+			const baseTo = radices[radix]
+			const converted = convertNum.from(baseFrom).to(baseTo)
 
-		responses.push(
-			`${currentArray.map(line => line.concat("\n")).join("")}`
-		)
+			output.push(`${converted} ${radix}`)
+		}
+
+		output.push("")
 	}
 
-	console.log(responses.join("\n"))
+	console.log(output.join("\n"))
 }
 
 main()
