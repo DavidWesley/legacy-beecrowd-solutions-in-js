811ca770e2e44c4e1283f9e9850cbaffe8349ef9
David Wesley <david.wesley755@outlook.com>
2022-07-29 23:22:28 -0300

    Refactor: Excluding Slice in 1222.js file
    Excluding slice methods associated to split methods

--- strings/1222.js
+++ strings/1222.js
@@ -1,22 +1,22 @@
 const { readFileSync } = require("fs")
-const lines = readFileSync("/dev/stdin", "utf8").split("\n")
+const input = readFileSync("/dev/stdin", "utf8").split("\n")
 
 function main() {
-	const responses = new Array()
+	const output = new Array()
 
-	for (let index = 0; index < lines.length; index += 2) {
-		if (lines[index] == "") break
+	for (let index = 0; index < input.length; index += 2) {
+		if (input[index] == "") break
 
-		const [L, C] = lines[index].split(" ").slice(1, 3).map(Number.parseFloat)
-		const text = lines[index + 1]
+		const [, L, C] = input[index].split(" ", 3).map(value => Number.parseInt(value, 10))
+		const text = input[index + 1]
 
 		const regex = RegExp(`\\b\\w[\\w ]{0,${C - 1}}\\b`, "gis")
 		const numPages = Math.ceil(text.match(regex).length / L)
 
-		responses.push(numPages)
+		output.push(numPages)
 	}
 
-	console.log(responses.join("\n"))
+	console.log(output.join("\n"))
 }
 
 main()
