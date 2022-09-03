c95e5d240a42e6b40b9ab64b5e65f9669fe29fa5
David Wesley <david.wesley755@outlook.com>
2022-09-03 18:01:39 -0300

    Add: 1215 - Primeiro Dicionário de Andy

---
+++ structure/1215.0.js
@@ -0,0 +1,25 @@
+const { readFileSync } = require("fs")
+const input = readFileSync("/dev/stdin", "utf8")
+	.split("\n")
+	.map(line => line.toLowerCase().split(/[\W\b\s_\d]+/gi))
+
+
+function main() {
+	const /** @type {Set<string>} */ uniqueWordsSet = new Set()
+
+	for (const line of input) {
+		for (const word of line) {
+			if (word === "") continue // Filtering empty strings
+			else uniqueWordsSet.add(word)
+		}
+	}
+
+	// Dictionary
+	const output = Array
+		.from(uniqueWordsSet.values())
+		.sort((strA, strB) => strA.localeCompare(strB, "en-US"))
+
+	console.log(output.join("\n"))
+}
+
+main()
