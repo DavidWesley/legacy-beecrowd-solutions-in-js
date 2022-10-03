4bbd7080b78e6bce1f4e23ef1f1e9b95a49b549b
David Wesley <david.wesley755@outlook.com>
2022-10-02 23:51:28 -0300

    Add: 1618 - Colisão

---
+++ adhoc/1618.js
@@ -0,0 +1,21 @@
+const { readFileSync } = require("node:fs")
+
+const [[numLines], ...input] = readFileSync("/dev/stdin", "utf8")
+	.split("\n")
+	.map((line) => line.split(" ", 10).map(value => Number.parseInt(value, 10)))
+
+function main() {
+	const output = []
+
+	for (let index = 0; index < numLines; index += 1) {
+		const [Ax, Ay, Bx, By, Cx, Cy, Dx, Dy, RX, RY] = input[index]
+
+		// Ax === Dx | Ay === By
+		// Bx === Cx | Dy === Cy
+		output.push((RX >= Ax && RX <= Cx) && (RY >= By && RY <= Dy) ? 1 : 0)
+	}
+
+	console.log(output.join("\n"))
+}
+
+main()
