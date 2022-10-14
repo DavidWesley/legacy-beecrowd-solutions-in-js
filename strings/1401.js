8260e96130b1704c7ca2f7639a471316342e6f2e
David Wesley <david.wesley755@outlook.com>
2022-10-14 02:58:53 -0300

    Refactor: 1401

--- strings/1401.js
+++ strings/1401.js
@@ -1,31 +1,42 @@
-const { readFileSync } = require("fs")
-const [numLines, ...lines] = readFileSync("/dev/stdin", "utf8").split("\n")
+const { readFileSync } = require("node:fs")
+const [numLines, ...input] = readFileSync("/dev/stdin", "utf8").split("\n")
 
-/** @returns {string[]} */
+/**
+ * Retorna uma lista de strings
+ * com todas as diferentes combinações possíveis
+ * entre os caracteres de `str` em que cada combinação
+ * tem tamanho igual a `str.length`
+ * @param {string} str
+ * @param {string} prefix
+ * @returns {string[]}
+*/
+function permutations(str, prefix = "") {
+	if (str.length <= 1) return [prefix + str]
 
-function getPermutations(string, prefix = "") {
-	if (string.length <= 1) return [prefix + string]
-
-	return Array.from(string).reduce((result, char, index) => {
-		const reminder = string.slice(0, index) + string.slice(index + 1)
-		return result.concat(getPermutations(reminder, prefix + char))
-	}, [])
+	return Array
+		.from(str)
+		.reduce((result, char, index) => {
+			const reminder = str.substring(0, index) + str.substring(index + 1)
+			return result.concat(permutations(reminder, prefix + char))
+		}, [])
 }
 
 function main() {
-	const responses = lines.slice(0, +numLines).flatMap((line) => {
-		const sorted = [...new Set(getPermutations(line)).values()].sort((strA, strB) => {
-			if (strA < strB) return -1
-			if (strA > strB) return 1
-			return 0
+	const output = input
+		.splice(0, Number.parseInt(numLines, 10))
+		.flatMap((line) => {
+			const sortedUniquePermutations = [...new Set(permutations(line)).values()]
+				.sort((strA, strB) => {
+					if (strA < strB) return -1
+					if (strA > strB) return 1
+					return 0
+				})
+
+			sortedUniquePermutations.push("")
+			return sortedUniquePermutations
 		})
 
-		sorted.push("")
-
-		return sorted
-	})
-
-	console.log(responses.join("\n"))
+	console.log(output.join("\n"))
 }
 
 main()
