ede2e37f644fc62c895c8324c7f08c16860fc840
David Wesley <david.wesley755@outlook.com>
2022-11-02 16:23:01 -0300

    Refact: 1096

--- beginner/1096.js
+++ beginner/1096.js
@@ -1,11 +1,11 @@
-const responses = []
+const output = []
 
 for (let i = 1; i <= 9; i += 2) {
-	responses.push(
+	output.push(
 		`I=${i} J=7`,
 		`I=${i} J=6`,
 		`I=${i} J=5`
 	)
 }
 
-console.log(responses.join("\n"))
+console.log(output.join("\n"))
