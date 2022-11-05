e6d8a7eb2ba43f04b863b428ec88dc7678bb8675
David Wesley <david.wesley755@outlook.com>
2022-11-05 17:01:40 -0300

    Update: 2991 - Estatísticas dos Departamentos

--- SQL/2991.sql
+++ SQL/2991.sql
@@ -1,52 +1,41 @@
-WITH
-brutes AS (
-	SELECT DISTINCT
-		empregado.matr,
-		-- vencimento.nome AS "venc_name",
-		-- vencimento.valor AS "venc_value",
-		COALESCE(SUM(vencimento.valor) OVER (PARTITION BY empregado.matr), 0) AS "brute"
-	FROM
-		empregado
-		INNER JOIN emp_venc ON emp_venc.matr = empregado.matr
-		INNER JOIN vencimento ON vencimento.cod_venc = emp_venc.cod_venc
-),
-discounts AS (
-	SELECT DISTINCT
-		empregado.matr,
-		-- desconto.nome AS "desc_name",
-		-- desconto.valor AS "desc_value",
-		COALESCE(SUM(desconto.valor) OVER (PARTITION BY empregado.matr), 0) AS "total_desc"
-	FROM
-		empregado
-		INNER JOIN emp_desc ON emp_desc.matr = empregado.matr
-		INNER JOIN desconto ON desconto.cod_desc = emp_desc.cod_desc
-),
-salaries AS (
-	SELECT DISTINCT
-		empregado.matr,
-		COALESCE(brutes.brute, 0),
-		COALESCE(discounts.total_desc, 0) AS "discount",
-		(COALESCE(brutes.brute, 0) - COALESCE(discounts.total_desc, 0)) AS "balance"
-	FROM
-		empregado
-		LEFT JOIN brutes ON empregado.matr = brutes.matr
-		LEFT JOIN discounts ON empregado.matr = discounts.matr
-)
-
-SELECT DISTINCT
-	departamento.nome AS "Nome Departamento",
-	COUNT(departamento.nome) OVER (PARTITION BY departamento.nome) AS "Numero de Empregados",
-	TRUNC(ROUND(AVG(salaries.balance) OVER (PARTITION BY departamento.nome), 2), 2) AS "Media Salarial",
-	TRUNC(ROUND(MAX(salaries.balance) OVER (PARTITION BY departamento.nome), 2), 2) AS "Maior Salario",
+SELECT
+	deps.nome AS "Nome Departamento",
+	COUNT(deps.nome) AS "Numero de Empregados",
+	ROUND(AVG(brutes.brute - discounts.total_desc), 2) AS "Media Salarial",
+	ROUND(MAX(brutes.brute - discounts.total_desc), 2) AS "Maior Salario",
 	(
 		CASE
-			WHEN MIN(salaries.balance) OVER (PARTITION BY departamento.nome) = 0 THEN 0
-			ELSE TRUNC(ROUND(MIN(salaries.balance) OVER (PARTITION BY departamento.nome), 2), 2)
+			WHEN MIN(brutes.brute - discounts.total_desc) = 0 THEN 0
+			ELSE ROUND(MIN(brutes.brute - discounts.total_desc), 2)
 		END
 	) AS "Menor Salario"
 FROM
-	empregado
-	LEFT JOIN salaries ON empregado.matr = salaries.matr
-	INNER JOIN departamento ON empregado.lotacao = departamento.cod_dep
+	departamento deps
+	INNER JOIN empregado emp ON deps.cod_dep = emp.lotacao
+	INNER JOIN (
+		SELECT
+			emp.matr,
+			COALESCE(SUM(vencimento.valor), 0) AS brute
+		FROM
+			empregado emp
+			LEFT JOIN emp_venc ON emp.matr = emp_venc.matr
+			LEFT JOIN vencimento ON emp_venc.cod_venc = vencimento.cod_venc
+		GROUP BY
+			emp.matr
+	) AS brutes ON emp.matr = brutes.matr
+	INNER JOIN (
+		SELECT
+			emp.matr,
+			COALESCE(SUM(desconto.valor), 0) AS total_desc
+		FROM
+			empregado emp
+			LEFT JOIN emp_desc ON emp.matr = emp_desc.matr
+			LEFT JOIN desconto ON emp_desc.cod_desc = desconto.cod_desc
+		GROUP BY
+			emp.matr
+	) AS discounts ON emp.matr = discounts.matr
+GROUP BY
+	deps.cod_dep,
+	deps.nome
 ORDER BY
-	"Media Salarial" DESC;
+	AVG(brutes.brute - discounts.total_desc) DESC
