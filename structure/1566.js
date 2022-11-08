ccca1b3cd97029ccaa179bdf7617e6370fbd6b2c
David Wesley <david.wesley755@outlook.com>
2022-11-07 21:58:30 -0300

    Refactor: 1566
    Adicionado a classe `LineReader`

--- structure/1566.js
+++ structure/1566.js
@@ -1,64 +1,86 @@
-//// READING FILE | STREAMS ////
 const { createReadStream } = require("node:fs")
 const { createInterface } = require("node:readline")
 
-const PATH = "/dev/stdin"
-const ENCODING = "utf8"
+//// READING FILE | STREAMS ////
+class LineReader {
+	/**
+   * @param {import("node:fs").PathLike} path
+   * @param {BufferEncoding} encoding
+   * @return {import("node:readline").ReadLine}
+   */
+	static createReadLineInterface(path, encoding = "utf8") {
+		const readStreamOptions = {
+			encoding: encoding,
+			flags: "r",
+			emitClose: true,
+			autoClose: true,
+		}
 
-/**
- * @param {string} path
- * @param {BufferEncoding} encoding
- */
-function createReadLineInterface(path, encoding) {
-	return createInterface({
-		input: createReadStream(path, encoding),
-		crlfDelay: Infinity,
-		terminal: false
-	})
-}
+		return createInterface({
+			input: createReadStream(path, readStreamOptions),
+			crlfDelay: Infinity,
+			terminal: false,
+		})
+	}
 
-/** @param {import("readline").Interface} readLineInterface */
-const processLineByLine = function (readLineInterface) {
-	let EOF = false
+	/**
+   * @param {import("node:fs").PathLike} path
+   * @param {BufferEncoding} encoding
+   */
+	static create(path, encoding) {
+		const RLI = LineReader.createReadLineInterface(path, encoding)
 
-	const nextLineGenerator = (async function* () {
-		for await (const line of readLineInterface) yield line
-		EOF = true
-	})()
+		let EOF = false
 
-	return {
-		hasNextLine: () => !EOF,
-		nextLine: async (fn) => {
-			const { value } = (await nextLineGenerator.next())
-			return (typeof fn === "function") ? fn(value) : value
+		const nextLineGenerator = (async function* () {
+			for await (const line of RLI) {
+				yield line
+			}
+		})()
+
+		RLI.once("close", () => {
+			EOF = true
+		})
+
+		return {
+			hasNextLine: () => !EOF,
+			nextLine: async (/** @type {unknown} */ fn) => {
+				const { value } = (await nextLineGenerator.next())
+				return (typeof fn === "function") ? fn(value) : value
+			},
+			close: () => RLI.close(),
 		}
 	}
 }
 
 async function main() {
-	const RLI = createReadLineInterface(PATH, ENCODING)
-	const readLineInstance = processLineByLine(RLI)
-	const nextLine = readLineInstance.nextLine.bind(undefined, (str = "") => str.split(" ").map(value => Number.parseInt(value, 10)))
+	const PATH = "/dev/stdin"
+	const ENCODING = "utf8"
+
+	const lineReader = LineReader.create(PATH, ENCODING)
 
 	// Número de caso de teste equivalente ao tamanho do array
 	// o qual será usado para impressão
-	const size = (await nextLine()).shift()
+	const size = Number.parseInt(await lineReader.nextLine(), 10)
 
 	// Criar um array in memory é mais rápido
 	// do que incrementar seu tamanho com o `.push` method
 	const output = new Array(size)
 
 	for (let index = 0; index < size; index++) {
-		// Desnecessário
-		// para otimização pode-se evitar atribuir essa linha a alguma variável
-		await nextLine()
+		const N = Number.parseInt(await lineReader.nextLine(), 10)
 
-		output[index] = await nextLine()
+		output[index] = (await lineReader.nextLine())
+			.split(" ", N)
+			.map((value) => Number.parseInt(value, 10))
 	}
+
+	if (lineReader.hasNextLine())
+		lineReader.close()
 
 	for (let index = 0; index < size; index++) {
 		// Nenhuma implementação hard-coded
-		// será mais perfomático que um método de sort nativo
+		// será mais performática que um método de sort nativo
 		output[index] = output[index].sort((a, b) => a - b).join(" ")
 	}
 
