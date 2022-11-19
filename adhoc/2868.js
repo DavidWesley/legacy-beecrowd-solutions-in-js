39954da1dcede4ea28666a10f601f56ed8d6ba11
David Wesley <david.wesley755@outlook.com>
2022-11-19 17:39:41 -0300

    Add: 2868 - Errrou!

---
+++ adhoc/2868.js
@@ -0,0 +1,34 @@
+const { readFileSync } = require("node:fs")
+const [numLines, ...input] = readFileSync("/dev/stdin", "utf8").split("\n")
+
+function evaluateSimpleLinearExpression(expression = "") {
+	while (expression.includes("("))
+		expression = expression
+			.replace(/\(([\d+\-*/ ]+?)\)/g, (_, op) => String(evaluateSimpleLinearExpression(op)))
+
+	expression = expression
+		.replace(/(-?\d+)\s*?([*/])\s?(-?\d+)/g, (_, numA, operator, numB) => {
+			if (operator === "*") return String(Number.parseInt(numA, 10) * Number.parseInt(numB, 10))
+			if (operator === "/") return String(Number.parseInt(numA, 10) / Number.parseInt(numB, 10))
+		})
+		.replace(/(-?\d+)\s*?([+-])\s*?(-?\d+)/g, (_, numA, operator, numB) => {
+			if (operator === "+") return String(Number.parseInt(numA, 10) + Number.parseInt(numB, 10))
+			if (operator === "-") return String(Number.parseInt(numA, 10) - Number.parseInt(numB, 10))
+		})
+
+	return Number(expression)
+}
+
+function main() {
+	const output = new Array(Number.parseInt(numLines, 10))
+
+	for (let index = 0; index < output.length; index++) {
+		const [exprA, exprB] = input[index].split("=", 2).map(e => e.replace("x", "*"))
+		const distance = Math.abs(evaluateSimpleLinearExpression(exprA) - evaluateSimpleLinearExpression(exprB))
+		output[index] = "Errou!".replace("rr", "r".repeat(distance))
+	}
+
+	console.log(output.join("\n"))
+}
+
+main()
