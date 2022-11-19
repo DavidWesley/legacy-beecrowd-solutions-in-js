7545913e9df7e59b33b7a784e2e732f2c636f72c
David Wesley <david.wesley755@outlook.com>
2022-11-19 01:03:49 -0300

    3076 - Exercício de História

---
+++ beginner/3076.js
@@ -0,0 +1,76 @@
+const { createReadStream } = require("node:fs")
+const { createInterface } = require("node:readline")
+
+//// READING FILE | STREAMS ////
+class LineReader {
+	/**
+	 * @param {import("node:fs").PathLike} path
+	 * @param {BufferEncoding} encoding
+	 * @return {import("node:readline").ReadLine}
+	 */
+	static createReadLineInterface(path, encoding = "utf8") {
+		const readStreamOptions = {
+			encoding: encoding,
+			flags: "r",
+			emitClose: true,
+			autoClose: true
+		}
+
+		return createInterface({
+			input: createReadStream(path, readStreamOptions),
+			crlfDelay: Infinity,
+			terminal: false
+		})
+	}
+
+	/**
+	 * @param {import("node:fs").PathLike} path
+	 * @param {BufferEncoding} encoding
+	 */
+	static create(path, encoding) {
+		const RLI = LineReader.createReadLineInterface(path, encoding)
+
+		let EOF = false
+
+		const nextLineGenerator = (async function* () {
+			for await (const line of RLI)
+				yield line
+		})()
+
+		RLI.once("close", () => { EOF = true })
+
+		return {
+			hasNextLine: () => !EOF,
+			nextLine: async (/** @type {unknown} */ fn) => {
+				const { value } = (await nextLineGenerator.next())
+				return (typeof fn === "function") ? fn(value) : value
+			},
+			close: () => RLI.close()
+		}
+	}
+}
+
+//// MAIN ////
+async function main() {
+	const PATH = "/dev/stdin"
+	const ENCODING = "utf8"
+
+	const output = []
+	const lineReader = LineReader.create(PATH, ENCODING)
+
+	const helper = (line = "") => Number.parseInt(line, 10)
+	const nextLine = lineReader.nextLine.bind(lineReader, helper)
+
+	while (lineReader.hasNextLine()) {
+		const N = await nextLine()
+		if (Number.isNaN(N)) break
+		else output.push(Math.ceil(N / 100))
+	}
+
+	if (lineReader.hasNextLine())
+		lineReader.close()
+
+	console.log(output.join("\n"))
+}
+
+main()
