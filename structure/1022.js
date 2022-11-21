4ebf61d0b8e11885830c8dcdecc5fdaefe2d2a73
David Wesley <david.wesley755@outlook.com>
2022-11-21 02:22:03 -0300

    Update(1022) - 1022 - TDA Racional (Clean & Optimized)

--- structure/1022.js
+++ structure/1022.js
@@ -1,84 +1,81 @@
-const { readFileSync } = require("fs")
-const [numTestCases, ...equations] = readFileSync("/dev/stdin", "utf8").split("\n")
+const { readFileSync } = require("node:fs")
+const [N, ...input] = readFileSync("/dev/stdin", "utf8").split("\n", 1 + 1e4)
 
-const FormattedEquationsFunctionsEnum = Object.freeze({
+//// MATH ////
+const MyMath = Object.create(Math, {
+	gcd: {
+		/**
+		 * @param {number} x
+		 * @param {number} y
+		 */
+		value: function (x, y) {
+			if (isNaN(x) || isNaN(y)) return Number.NaN
+			x = Math.abs(x)
+			y = Math.abs(y)
+			while (y) [x, y] = [y, x % y]
+			return x
+		},
+		configurable: false,
+		enumerable: false,
+		writable: false
+	}
+})
+
+//// FRACTION ////
+const FormattedFractionalEquationsFunctionsEnum = Object.freeze({
 	sum: (N1 = 1, D1 = 1, N2 = 0, D2 = 0) => `${(N1 * D2 + N2 * D1)}/${(D1 * D2)}`,
 	subtract: (N1 = 1, D1 = 1, N2 = 0, D2 = 0) => `${(N1 * D2 - N2 * D1)}/${(D1 * D2)}`,
 	multiply: (N1 = 0, D1 = 0, N2 = 1, D2 = 1) => `${(N1 * N2)}/${(D1 * D2)}`,
 	divide: (N1 = 0, D1 = 0, N2 = 1, D2 = 1) => `${(N1 * D2)}/${(N2 * D1)}`,
 })
 
-// Run Call all function here
+function destructureSimpleFractionalLinearEquation(equation = "") {
+	return equation
+		.replace(/\s+/g, "")
+		.match(/(\d+)\/(\d+)([+\-*/])(\d+)\/(\d+)/s)
+		.slice(1, 6)
+}
 
+/**
+ * Set the correct operation from equation input
+ * @param {number} N1
+ * @param {number} D1
+ * @param {string} operator
+ * @param {number} N2
+ * @param {number} D2
+ */
+function fractionalOperation(N1, D1, operator, N2, D2) {
+	switch (operator) {
+		case "+": return FormattedFractionalEquationsFunctionsEnum.sum(N1, D1, N2, D2)
+		case "-": return FormattedFractionalEquationsFunctionsEnum.subtract(N1, D1, N2, D2)
+		case "*": return FormattedFractionalEquationsFunctionsEnum.multiply(N1, D1, N2, D2)
+		case "/": return FormattedFractionalEquationsFunctionsEnum.divide(N1, D1, N2, D2)
+		default: return FormattedFractionalEquationsFunctionsEnum.multiply(N1, D1)
+	}
+}
+
+/**
+ * @param {number} N
+ * @param {number} D
+ */
+function reduceFraction(N, D) {
+	const divisor = MyMath.gcd(N, D)
+	return [N / divisor, D / divisor]
+}
+
+//// MAIN ////
 function main() {
-	const responses = equations.slice(0, +numTestCases).map(resolveEquation)
-	console.log(responses.join("\n"))
+	const output = new Array(Number.parseInt(N, 10))
+
+	for (let index = 0; index < output.length; index++) {
+		const [nA, dA, op, nB, dB] = destructureSimpleFractionalLinearEquation(input[index])
+		const [niA, diA] = fractionalOperation(+nA, +dA, op, +nB, +dB).split("/", 2).map(value => Number.parseInt(value, 10))
+		const [nrA, drA] = reduceFraction(niA, diA)
+
+		output[index] = `${niA}/${diA} = ${nrA}/${drA}`
+	}
+
+	console.log(output.join("\n"))
 }
 
 main()
-
-
-// Return the final equation solved
-
-/** @param {string} equation */
-
-function resolveEquation(equation) {
-	const unstructured = destructSimpleLinearEquation(equation)
-	const initialFraction = defineOperation(unstructured)
-	const reducedFraction = defineOperation(reduceFraction(initialFraction))
-
-	return `${initialFraction} = ${reducedFraction}`
-}
-
-
-// Functions Destructs
-
-/** @param { string } fraction */
-
-function destructFraction(fraction) {
-	return fraction.replace(/\s/gs, "").split("/").map(Number.parseFloat)
-}
-
-/** @param {string} equation */
-
-function destructSimpleLinearEquation(equation) {
-	const equationRegex = /(\d+)\/(\d+)([+\-*/])(\d+)\/(\d+)/s
-	return equation.replace(/\s/g, "").match(equationRegex).slice(1, 5)
-}
-
-
-// Set the correct operation from equation input
-
-/** @param {[number, number, string, number, number]} equation */
-
-function defineOperation(equation) {
-	const [N1, D1, operator, N2, D2] = equation
-
-	if (operator === "+") return FormattedEquationsFunctionsEnum.sum(N1, D1, N2, D2)
-	else if (operator === "-") return FormattedEquationsFunctionsEnum.subtract(N1, D1, N2, D2)
-	else if (operator === "*") return FormattedEquationsFunctionsEnum.multiply(N1, D1, N2, D2)
-	else if (operator === "/") return FormattedEquationsFunctionsEnum.divide(N1, D1, N2, D2)
-
-	return FormattedEquationsFunctionsEnum.multiply(N1, D1)
-}
-
-
-// Return the fraction formatted
-
-/** @param {string} fraction */
-
-function reduceFraction(fraction) {
-	const [N, D] = destructFraction(fraction)
-	const MDC = GCD([N, D])
-
-	return [N / MDC, D / MDC]
-}
-
-// Aditionals Functions
-
-const GCD = ([x, y]) => {
-	if (isNaN(x) || isNaN(y)) return
-	x = Math.abs(x), y = Math.abs(y)
-	while (y !== 0) [x, y] = [y, x % y]
-	return x
-}
