5189db2fcd60ef3bcee981e36eac1295723869d4
David Wesley <david.wesley755@outlook.com>
2022-11-20 23:49:02 -0300

    Refactor(1234) - Simplificando a função de formatação

--- strings/1234.js
+++ strings/1234.js
@@ -1,29 +1,28 @@
-const { readFileSync } = require("fs")
-const lines = readFileSync("/dev/stdin", "utf8").split("\n")
+const { readFileSync } = require("node:fs")
+const input = readFileSync("/dev/stdin", "utf8").split("\n")
 
-function convertStringToAlternatingCase(sequence = "", from = 0) {
-	const prequel = sequence.substring(0, from)
-	const posquel = sequence.slice(from).replace(/[a-z]+/gi, (match) => {
-		const formatted = Array.from(match, (char, index) => (index + from) % 2 == 0 ? char.toUpperCase() : char.toLowerCase()).join("")
-		from += formatted.length
+function convertStringToContinuosAlternatingCase(str = "", from = 0) {
+	const arr = [...str]
 
-		return formatted
-	})
+	for (let index = from, toUpper = true; index < arr.length; index++) {
+		const char = arr[index]
+		if (/[a-z]/i.test(char)) {
+			arr[index] = toUpper ? char.toUpperCase() : char.toLowerCase()
+			toUpper = !toUpper
+		}
+	}
 
-	return `${prequel}${posquel}`
+	return arr.join("")
 }
 
 function main() {
-	const responses = []
+	const output = []
 
-	for (const sentence of lines) {
-		if (sentence == "") break // EOFile Condilion
-		const dancingSentence = convertStringToAlternatingCase(sentence, 0)
+	for (const line of input)
+		if (line == "") break // EOFile Condition
+		else output.push(convertStringToContinuosAlternatingCase(line, 0))
 
-		responses.push(dancingSentence)
-	}
-
-	console.log(responses.join("\n"))
+	console.log(output.join("\n"))
 }
 
 main()
