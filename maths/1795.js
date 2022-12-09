d4be2dcfcc59438b28cda7539b96b7901058bd13
David Wesley <david.wesley755@outlook.com>
2022-12-09 19:07:53 -0300

    Update: 1795 - Triângulo Trinomial
    Aumentando performance
    Reduzindo robustez
    Remoção da função `Pow`

--- maths/1795.js
+++ maths/1795.js
@@ -1,6 +1,6 @@
-const { readFileSync } = require("fs")
-const [lineIndex] = readFileSync("/dev/stdin", "utf8").split("\n")
+const { readFileSync } = require("node:fs")
+const [R] = readFileSync("/dev/stdin", "utf8")
+	.split("\n", 1)
+	.map(value => Number.parseInt(value, 10))
 
-const Pow = (base, exponent) => BigInt(base) ** BigInt(exponent)
-
-console.log(Pow(3, lineIndex))
+console.log(Math.pow(3, R))
