a31e38a365a8eafc5a75299a93e913341e152102
David Wesley <david.wesley755@outlook.com>
2022-12-25 01:57:11 -0300

    Update(1146)
    Substituindo criação por iteração de lista por uma buffer de tamanho não excessivo.

--- beginner/1146.js
+++ beginner/1146.js
@@ -1,16 +1,19 @@
-const { readFileSync } = require("node:fs");
+const { readFileSync } = require("node:fs")
 const input = readFileSync("/dev/stdin", "utf8")
 	.split("\n")
-	.map(value => Number.parseInt(value, 10));
+	.map(value => Number.parseInt(value, 10))
 
 function main() {
-	const output = [];
+	const MAX_SIZE = 1e3
+
+	const output = []
+	const buffer = new Uint16Array(MAX_SIZE).map((_, i) => i + 1)
 
 	for (const size of input)
-		if (size === 0) break;
-		else output.push(Array.from({ length: size }, (_, i) => i + 1).join(" "));
+		if (size === 0) break
+		else output.push(buffer.subarray(0, size).join(" "))
 
-	console.log(output.join("\n"));
+	console.log(output.join("\n"))
 }
 
-main();
+main()
