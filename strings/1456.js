6fdd0fb5d5e4ecd79dffc8ffb6c6b96f552c0110
David Wesley <david.wesley755@outlook.com>
2022-12-28 14:37:54 -0300

    refactor(1456): :recycle: Adding LineReader class

--- strings/1456.js
+++ strings/1456.js
@@ -1,38 +1,113 @@
-const { readFileSync } = require("fs")
-const [numCases, ...lines] = readFileSync("/dev/stdin", "utf8").split("\n")
+const { createReadStream } = require("node:fs")
+const { createInterface } = require("node:readline")
 
-function main() {
-	const responses = []
+//// READING FILE | STREAMS ////
+class LineReader {
+	/**
+	 * @param {import("node:fs").PathLike} path
+	 * @param {BufferEncoding} encoding
+	 * @return {import("node:readline").ReadLine}
+	 */
+	static createReadLineInterface(path, encoding = "utf8") {
+		const readStreamOptions = {
+			encoding: encoding,
+			flags: "r",
+			emitClose: true,
+			autoClose: true
+		}
 
-	for (let instanceIndex = 0; instanceIndex < +numCases; instanceIndex++) {
-		const start = 3 * instanceIndex
-		const end = start + 3
+		return createInterface({
+			input: createReadStream(path, readStreamOptions),
+			crlfDelay: Infinity,
+			terminal: false
+		})
+	}
 
-		const [, input, code] = lines.slice(start, end)
-		const decodedText = BrainfuckIntepreter(code, input)
+	/**
+	 * @param {import("node:fs").PathLike} path
+	 * @param {BufferEncoding} encoding
+	 */
+	static create(path, encoding) {
+		const RLI = LineReader.createReadLineInterface(path, encoding)
 
-		responses.push(
-			`Instancia ${instanceIndex + 1}`,
-			`${decodedText}`,
+		let EOF = false
+
+		const nextLineGenerator = (async function* () {
+			for await (const line of RLI)
+				yield line
+		})()
+
+		RLI.once("close", () => { EOF = true })
+
+		return {
+			hasNextLine: () => !EOF,
+			nextLine: async (/** @type {unknown} */ fn) => {
+				const { value } = (await nextLineGenerator.next())
+				return (typeof fn === "function") ? fn(value) : value
+			},
+			close: () => RLI.close()
+		}
+	}
+}
+
+async function main() {
+	const PATH = "/dev/stdin"
+	const ENCODING = "utf8"
+
+	const output = []
+	const lineReader = LineReader.create(PATH, ENCODING)
+
+	const N = Number.parseInt(await lineReader.nextLine(), 10)
+
+	for (let index = 1; lineReader.hasNextLine() && index <= N; index++) {
+		await lineReader.nextLine() // skipping blank line
+		const input = await lineReader.nextLine()
+		const code = await lineReader.nextLine()
+
+		const decodedText = BrainfuckInterpreter(code, input)
+
+		output.push(
+			`Instancia ${index}`,
+			decodedText,
 			""
 		)
 	}
 
-	console.log(responses.join("\n"))
+	if (lineReader.hasNextLine())
+		lineReader.close()
+
+	console.log(output.join("\n"))
 }
 
 main()
 
-// Inspered By Code of Harley Jackson
+/*
+┌─────────────┬────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────┐
+│  Instrução  │                                                             Descrição                                                              │
+├─────────────┼────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────┤
+│      >      │ Incrementa o ponteiro.                                                                                                             │
+│      <      │ Decrementa o ponteiro.                                                                                                             │
+│      +      │ Incrementa o byte na posição indicada pelo ponteiro.                                                                               │
+│      -      │ Decrementa o byte na posição indicada pelo ponteiro.                                                                               │
+│      .      │ Imprime o valor do byte na posição indicada pelo ponteiro.                                                                         │
+│      ,      │ Lê um byte e armazena na posição indicada pelo ponteiro. Se não houver nada que possa ser lido (entrada acabou), armazenar zero.   │
+│      [      │ Início do loop: Executa o código delimitado até que o byte na posição indicada pelo ponteiro seja igual a zero.                    │
+│      ]      │ Fim do loop.                                                                                                                       │
+│      #      │ Imprime os valores das 10 primeiras posições do vetor.                                                                             │
+└─────────────┴────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────┘
+*/
 
-/** @param {string} code */
 
-function BrainfuckIntepreter(code, input = "") {
+/**
+ * Inspired By Code of Harley Jackson
+ * @param {string} code
+ */
+function BrainfuckInterpreter(code, input = "") {
 	const buff = new Uint8Array(new SharedArrayBuffer(3e4))
 	const loops = new Array(0)
 	const chars = new Array(0)
 
-	// progamIndex | buffIndex | inputindex
+	// programIndex | buffIndex | inputIndex
 	let [pIndex, bIndex, iIndex] = [0, 0, 0]
 
 	// Reading code and starting some configurations
