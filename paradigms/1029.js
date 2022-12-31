465cb60768d57ed17c2fcf7ad9c01af9d22b9f2a
David Wesley <david.wesley755@outlook.com>
2022-12-31 00:18:49 -0300

    refactor: :recycle: Short refactor code
    Fixing wrongs typo in function name
    Added `node:` prefix for built-in node modules

--- paradigms/1029.js
+++ paradigms/1029.js
@@ -1,62 +1,59 @@
-/**
- * FUNC   |   CALLS   | VALUE |
- * fib( 0) =   0 calls =   0
- * fib( 1) =   0 calls =   1
- * fib( 2) =   2 calls =   1
- * fib( 3) =   4 calls =   2
- * fib( 4) =   8 calls =   3
- * fib( 5) =  14 calls =   5
- * fib( 6) =  24 calls =   8
- * fib( 7) =  40 calls =   13
- * fib( 8) =  66 calls =   21
- * fib( 9) = 108 calls =   34
- * fib(10) = 176 calls =   55
- */
+const { format } = require("node:util")
+const { readFileSync } = require("node:fs")
+const [N, ...input] = readFileSync("/dev/stdin", "utf8")
+	.split("\n")
+	.map(value => Number.parseInt(value, 10))
+
+/*
+┌─────┬───────┬───────┐
+│ FIB │ CALLS │ VALUE │
+├─────┼───────┼───────┤
+│  0  │   0   │   0   │
+│  1  │   0   │   1   │
+│  2  │   2   │   1   │
+│  3  │   4   │   2   │
+│  4  │   8   │   3   │
+│  5  │   14  │   5   │
+│  6  │   24  │   8   │
+│  7  │   40  │   13  │
+│  8  │   66  │   21  │
+│  9  │  108  │   34  │
+│  10 │  176  │   55  │
+└─────┴───────┴───────┘
+*/
 
 /**
- * Call FIB function:
- * * C(n) = [ (n - 2 > 0) ? ( C(n - 2) + 1 ) : (return 0) ) ] + [ (n - 1 > 0) ? C(n - 1) + 1 : (return 0) ]
- * * F(n) = F(n - 2) + F(n - 1)
+ * @param {number} nth
+ * @param {Map<number, { calls: number, value: number }>} memo
  */
-
-const { format } = require("util")
-const { readFileSync } = require("fs")
-
-const [numLines, ...input] = readFileSync("/dev/stdin", "utf8").split("\n")
-
-/** @typedef {{ calls: number, value: number }} countableFibProp */
-
-/** @type { Map<number, countableFibProp> } */
-const FibonacciList = new Map([
-	[0, { calls: 0, value: 0 }],
-	[1, { calls: 0, value: 1 }],
-	[2, { calls: 2, value: 1 }]
-])
-
-
-function recusiveFibonacciWithCallsCounter(nth = 0) {
+function recursiveFibonacciWithCallsCounter(nth = 0, memo) {
 	nth = Math.floor(Math.max(0, nth)) // is nth leq 0? So set nth to 0
 
-	if (FibonacciList.has(nth) === false) {
-		// If not included, set it
-		const fibA = recusiveFibonacciWithCallsCounter(nth - 2)
-		const fibB = recusiveFibonacciWithCallsCounter(nth - 1)
+	if (memo.has(nth) === false) {
+		const fibA = recursiveFibonacciWithCallsCounter(nth - 2, memo)
+		const fibB = recursiveFibonacciWithCallsCounter(nth - 1, memo)
 
-		FibonacciList.set(nth, {
+		memo.set(nth, {
 			calls: (fibA.calls + 1) + (fibB.calls + 1),
 			value: fibA.value + fibB.value
 		})
 	}
 
-	return FibonacciList.get(nth)
+	return memo.get(nth)
 }
 
 function main() {
-	const output = new Array(Number.parseInt(numLines, 10))
+	const MEMO = new Map([
+		[0, { calls: 0, value: 0 }],
+		[1, { calls: 0, value: 1 }],
+		[2, { calls: 2, value: 1 }]
+	])
 
-	for (let index = 0; index < output.length; index += 1) {
-		const nth = Number.parseInt(input[index])
-		const { calls, value } = recusiveFibonacciWithCallsCounter(nth)
+	const output = new Array(N)
+
+	for (let index = 0; index < N; index += 1) {
+		const nth = input[index]
+		const { calls, value } = recursiveFibonacciWithCallsCounter(nth, MEMO)
 		output[index] = format("fib(%d) = %d calls = %d", nth, calls, value)
 	}
 
