2475a34b67eea09f435fa2962f6895d7384fae81
David Wesley <david.wesley755@outlook.com>
2023-02-05 14:36:16 -0300

    Add: 1542 - Lendo Livros

---
+++ adhoc/1542.js
@@ -0,0 +1,82 @@
+const { createReadStream } = require("node:fs")
+const { createInterface } = require("node:readline")
+
+//// READING FILE | STREAMS ////
+class LineReader {
+	/**
+	 * @param {import("node:fs").PathLike} path
+	 * @param {BufferEncoding} encoding
+	 * @return {import("node:readline").ReadLine}
+	 */
+	static createReadLineInterface(path, encoding = "utf8") {
+		const readStreamOptions = {
+			encoding: encoding,
+			flags: "r",
+			emitClose: true,
+			autoClose: true
+		}
+
+		return createInterface({
+			input: createReadStream(path, readStreamOptions),
+			crlfDelay: Infinity,
+			terminal: false
+		})
+	}
+
+	/**
+	 * @param {import("node:fs").PathLike} path
+	 * @param {BufferEncoding} encoding
+	 */
+	static create(path, encoding) {
+		const RLI = LineReader.createReadLineInterface(path, encoding)
+
+		let EOF = false
+
+		const nextLineGenerator = (async function* () {
+			for await (const line of RLI)
+				yield line
+		})()
+
+		RLI.once("close", () => { EOF = true })
+
+		return {
+			hasNextLine: () => !EOF,
+			nextLine: async (/** @type {unknown} */ fn) => {
+				const { value } = (await nextLineGenerator.next())
+				return (typeof fn === "function") ? fn(value) : value
+			},
+			close: () => RLI.close()
+		}
+	}
+}
+
+async function main() {
+	const PATH = "/dev/stdin"
+	const ENCODING = "utf8"
+
+	const output = []
+	const lineReader = LineReader.create(PATH, ENCODING)
+
+	const helper = (line = "") => line.split(" ", 3).map(value => Number.parseInt(value, 10))
+	const nextLine = lineReader.nextLine.bind(lineReader, helper)
+
+	while (lineReader.hasNextLine()) {
+		const [Q, D = 0, P = 0] = await nextLine()
+
+		if (Q === 0) break
+		const bookPagesQuantity = Math.floor((Q * D * P) / (P - Q))
+
+		output.push(
+			bookPagesQuantity === 1 ?
+				`${bookPagesQuantity} pagina` :
+				`${bookPagesQuantity} paginas`
+		)
+	}
+
+	if (lineReader.hasNextLine())
+		lineReader.close()
+
+	console.log(output.join("\n"))
+}
+
+main()
